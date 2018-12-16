4f65d49c1804677055e2dad8ed3705348e16de62
emiracle <ekaymakoglu@gmail.com>
2018-12-16 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4746,
-	"created_at": "Sat Dec 15 01:16:55 +0000 2018",
+	"tweets": 4747,
+	"created_at": "Sun Dec 16 01:16:55 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
+		"created_at": "Sat Dec 15 08:25:42 +0000 2018",
+		"id": 1073856613197459500,
+		"id_str": "1073856613197459457",
+		"full_text": "@umur_kara Ali Koç'un Fenerbahçeliliğinden şüphem yok ama acemilikler yaptığının da farkındaydım. Fenerbahçe'yi yönetmek ateşten bir gömlek. Sırf Aziz Yıldırım sultasını yıktığı için bile orta vadede kendisini eleştirmek gelmiyor içimden.",
+		"truncated": false,
+		"display_text_range": [
+			11,
+			238
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "umur_kara",
+					"name": "umur kara",
+					"id": 2384089392,
+					"id_str": "2384089392",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://www.samruston.co.uk\" rel=\"nofollow\">Flamingo for Android</a>",
+		"in_reply_to_status_id": 1073692905838182400,
+		"in_reply_to_status_id_str": "1073692905838182400",
+		"in_reply_to_user_id": 2384089392,
+		"in_reply_to_user_id_str": "2384089392",
+		"in_reply_to_screen_name": "umur_kara",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 278,
+			"friends_count": 235,
+			"listed_count": 9,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 702,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4668,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@umur_kara Ali Koç'un Fenerbahçeliliğinden şüphem yok ama acemilikler yaptığının da farkındaydım. Fenerbahçe'yi yönetmek ateşten bir gömlek. Sırf Aziz Yıldırım sultasını yıktığı için bile orta vadede kendisini eleştirmek gelmiyor içimden."
+	},
+	{
 		"created_at": "Fri Dec 14 19:52:18 +0000 2018",
 		"id": 1073667012717674500,
 		"id_str": "1073667012717674504",
