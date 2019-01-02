cb75261f4eb1358976aadfd1626c13b3ec67a284
emiracle <ekaymakoglu@gmail.com>
2019-01-02 04:16:56 +0300

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4755,
-	"created_at": "Tue Jan 01 01:16:55 +0000 2019",
+	"tweets": 4758,
+	"created_at": "Wed Jan 02 01:16:55 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_01.js
@@ -0,0 +1,279 @@
+Grailbird.data.tweets_2019_01 = 
+[
+	{
+		"created_at": "Tue Jan 01 16:18:46 +0000 2019",
+		"id": 1080136258754695200,
+		"id_str": "1080136258754695170",
+		"full_text": "Suriyeliler 5-6 yıldır buradalar. Şanslı olanların çocukları okula gidiyor, kendileri işe. Senden benden bir farkları yok. Eğlenmişler diye “savaşın ortasında ne eğlencesi” demenin bir anlamı yok. Ne mutlu bize ki; kendilerini evde hissetmiş eğlenmişler.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			254
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 286345414,
+			"id_str": "286345414",
+			"name": "Salieri",
+			"screen_name": "Salieri_",
+			"location": "Liberal",
+			"description": "Bana güvenip kimse ülke idare etmeye, devrim yapmaya kalkışmasın. Sonra küsüyorsunuz. 🚴🏻",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 32880,
+			"friends_count": 400,
+			"listed_count": 275,
+			"created_at": "Fri Apr 22 20:31:29 +0000 2011",
+			"favourites_count": 12503,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 111068,
+			"lang": "tr",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/993428247118598145/xxQMZ09M_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/993428247118598145/xxQMZ09M_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/286345414/1506320276",
+			"profile_link_color": "2C95B8",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DAECF4",
+			"profile_text_color": "663B12",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 53,
+		"favorite_count": 400,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "tr",
+		"text": "Suriyeliler 5-6 yıldır buradalar. Şanslı olanların çocukları okula gidiyor, kendileri işe. Senden benden bir farkları yok. Eğlenmişler diye “savaşın ortasında ne eğlencesi” demenin bir anlamı yok. Ne mutlu bize ki; kendilerini evde hissetmiş eğlenmişler."
+	},
+	{
+		"created_at": "Tue Jan 01 15:45:07 +0000 2019",
+		"id": 1080127790832332800,
+		"id_str": "1080127790832332801",
+		"full_text": "50 Soruda Yapay Zeka, bir tweet'te gerizeka! Türk ırkçılarının en temel özelliği, kendi ırkçılıklarının asla farkında olmamalarıdır. Türk toplumuna yakışan bir ikiyüzlülük. https://t.co/YaiovO8OfL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			172
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/YaiovO8OfL",
+					"expanded_url": "https://twitter.com/say_cem/status/1080035371440840704",
+					"display_url": "twitter.com/say_cem/status…",
+					"indices": [
+						173,
+						196
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.samruston.co.uk\" rel=\"nofollow\">Flamingo for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 277,
+			"friends_count": 237,
+			"listed_count": 9,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 714,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4678,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1080035371440840700,
+		"quoted_status_id_str": "1080035371440840704",
+		"quoted_status_permalink": {
+			"url": "https://t.co/YaiovO8OfL",
+			"expanded": "https://twitter.com/say_cem/status/1080035371440840704",
+			"display": "twitter.com/say_cem/status…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Jan 01 09:37:53 +0000 2019",
+			"id": 1080035371440840700,
+			"id_str": "1080035371440840704",
+			"full_text": "Akla mantığa aykırı bir politikayla buyur edilen tiplerin bizim askerimiz onların ülkesinde can verirken Taksim'de bayrak açmasına laf etmeyi ırkçılık sanacak boyutta cehalet ancak eğitimle olur.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				195
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 505670972,
+				"id_str": "505670972",
+				"name": "Cem Say",
+				"screen_name": "say_cem",
+				"location": "http://odatv.com/yazar/cemsay",
+				"description": "Bilgisayar mühendisliği hocası, \"50 Soruda Yapay Zekâ\" kitabının yazarı.   \n\n\n\n \n \n \nMahkeme kararıyla yalanlar gerçeğe dönüşseydi Dünya şimdi dönmüyordu.",
+				"url": "https://t.co/4PKyocNifD",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/4PKyocNifD",
+								"expanded_url": "http://www.herkesebilimteknoloji.com/k/yazarlar/cem-say",
+								"display_url": "herkesebilimteknoloji.com/k/yazarlar/cem…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 90942,
+				"friends_count": 777,
+				"listed_count": 305,
+				"created_at": "Mon Feb 27 09:07:09 +0000 2012",
+				"favourites_count": 8269,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 90410,
+				"lang": "tr",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1048601363612160000/gEQXhLwi_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1048601363612160000/gEQXhLwi_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/505670972/1539968899",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1494,
+			"favorite_count": 6865,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "tr"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "50 Soruda Yapay Zeka, bir tweet'te gerizeka! Türk ırkçılarının en temel özelliği, kendi ırkçılıklarının asla farkında olmamalarıdır. Türk toplumuna yakışan bir ikiyüzlülük. https://t.co/YaiovO8OfL"
+	}
+]
