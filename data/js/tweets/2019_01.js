7d4302a999c08c14553c078d12f97f62db7b0ede
emiracle <ekaymakoglu@gmail.com>
2019-01-05 04:16:56 +0300

    Added 1 tweets

--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Fri Jan 04 19:33:08 +0000 2019",
+		"id": 1081272335695519700,
+		"id_str": "1081272335695519745",
+		"full_text": "@bunal Bulunsa bile o link'ler bir işe yarıyor mu? Bana bir işe yaramıyormuş gibi geliyor.",
+		"truncated": false,
+		"display_text_range": [
+			7,
+			90
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "bunal",
+					"name": "Berkay ÜNAL",
+					"id": 7526632,
+					"id_str": "7526632",
+					"indices": [
+						0,
+						6
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://www.samruston.co.uk\" rel=\"nofollow\">Flamingo for Android</a>",
+		"in_reply_to_status_id": 1081181782882598900,
+		"in_reply_to_status_id_str": "1081181782882598913",
+		"in_reply_to_user_id": 7526632,
+		"in_reply_to_user_id_str": "7526632",
+		"in_reply_to_screen_name": "bunal",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 278,
+			"friends_count": 238,
+			"listed_count": 9,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 716,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4683,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@bunal Bulunsa bile o link'ler bir işe yarıyor mu? Bana bir işe yaramıyormuş gibi geliyor."
+	},
+	{
 		"created_at": "Wed Jan 02 14:00:20 +0000 2019",
 		"id": 1080463806667604000,
 		"id_str": "1080463806667603968",
