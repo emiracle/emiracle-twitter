eb1eda0ee3290685eff68636f61134150f791433
emiracle <ekaymakoglu@gmail.com>
2019-01-06 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4766,
-	"created_at": "Sat Jan 05 01:16:56 +0000 2019",
+	"tweets": 4767,
+	"created_at": "Sun Jan 06 01:16:55 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Thu Mar 30 14:08:59 +0000 2017",
+		"id": 847450585142112300,
+		"id_str": "847450585142112256",
+		"full_text": "Sartre, Neruda ve Beauvoir'un TRye girişleri yasakmış https://t.co/HA9YyF7OaN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			53
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 847450583380430800,
+					"id_str": "847450583380430848",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/C8K_0MsWkAAdEw_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C8K_0MsWkAAdEw_.jpg",
+					"url": "https://t.co/HA9YyF7OaN",
+					"display_url": "pic.twitter.com/HA9YyF7OaN",
+					"expanded_url": "https://twitter.com/dogangurpinar/status/847450585142112256/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 336,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 223,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 336,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 847450583380430800,
+					"id_str": "847450583380430848",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/C8K_0MsWkAAdEw_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C8K_0MsWkAAdEw_.jpg",
+					"url": "https://t.co/HA9YyF7OaN",
+					"display_url": "pic.twitter.com/HA9YyF7OaN",
+					"expanded_url": "https://twitter.com/dogangurpinar/status/847450585142112256/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 336,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 223,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 336,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 85599902,
+			"id_str": "85599902",
+			"name": "Dogan Gurpinar",
+			"screen_name": "dogangurpinar",
+			"location": "",
+			"description": "hamasete sıfır tolerans İstanbul Teknik Üniversitesi",
+			"url": "https://t.co/VlBOq2Wlnc",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/VlBOq2Wlnc",
+							"expanded_url": "http://akademi.itu.edu.tr/dgurpinar/",
+							"display_url": "akademi.itu.edu.tr/dgurpinar/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7526,
+			"friends_count": 488,
+			"listed_count": 93,
+			"created_at": "Tue Oct 27 16:05:57 +0000 2009",
+			"favourites_count": 9415,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 56130,
+			"lang": "tr",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1039171215473160192/Ht5vUhfa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1039171215473160192/Ht5vUhfa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/85599902/1408920828",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 62,
+		"favorite_count": 130,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "Sartre, Neruda ve Beauvoir'un TRye girişleri yasakmış https://t.co/HA9YyF7OaN"
+	},
+	{
 		"created_at": "Fri Jan 04 19:33:08 +0000 2019",
 		"id": 1081272335695519700,
 		"id_str": "1081272335695519745",
