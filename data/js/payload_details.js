732e9806202a3098ba5359ae7421c43b30f62329
emiracle <ekaymakoglu@gmail.com>
2019-01-21 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5931,
-	"created_at": "Sat Jan 19 01:16:55 +0000 2019",
+	"tweets": 5932,
+	"created_at": "Mon Jan 21 01:16:55 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Sun Jan 20 15:02:43 +0000 2019",
+		"id": 1087002490632052700,
+		"id_str": "1087002490632052738",
+		"full_text": "@Gamsizm Futbolla alakan da bu yıllarda kalmıştır diye düşünüyorum :)",
+		"truncated": false,
+		"display_text_range": [
+			9,
+			69
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Gamsizm",
+					"name": "Murat Gamsız",
+					"id": 41779319,
+					"id_str": "41779319",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://www.samruston.co.uk\" rel=\"nofollow\">Flamingo for Android</a>",
+		"in_reply_to_status_id": 1086988538795700200,
+		"in_reply_to_status_id_str": "1086988538795700224",
+		"in_reply_to_user_id": 41779319,
+		"in_reply_to_user_id_str": "41779319",
+		"in_reply_to_screen_name": "Gamsizm",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 285,
+			"friends_count": 237,
+			"listed_count": 9,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 735,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4694,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@Gamsizm Futbolla alakan da bu yıllarda kalmıştır diye düşünüyorum :)"
+	},
+	{
 		"created_at": "Fri Jan 18 06:51:11 +0000 2019",
 		"id": 1086154015451299800,
 		"id_str": "1086154015451299840",
@@ -192,7 +285,7 @@ Grailbird.data.tweets_2019_01 =
 		"created_at": "Thu Jan 17 18:25:29 +0000 2019",
 		"id": 1085966355080335400,
 		"id_str": "1085966355080335361",
-		"full_text": "@Gamsizm What can you do sometimes? 🤷‍♂️ https://t.co/K1LkBeKCyM",
+		"full_text": "@Gamsizm What can you do sometimes? 🤷♂️ https://t.co/K1LkBeKCyM",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -442,7 +535,7 @@ Grailbird.data.tweets_2019_01 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "@Gamsizm What can you do sometimes? 🤷‍♂️ https://t.co/K1LkBeKCyM"
+		"text": "@Gamsizm What can you do sometimes? 🤷♂️ https://t.co/K1LkBeKCyM"
 	},
 	{
 		"created_at": "Thu Jan 17 08:57:58 +0000 2019",
