ad48170772db1cc039514acbb9bff75185199941
emiracle <ekaymakoglu@gmail.com>
2019-02-07 04:16:57 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5939,
-	"created_at": "Wed Feb 06 01:16:57 +0000 2019",
+	"tweets": 5940,
+	"created_at": "Thu Feb 07 01:16:56 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -78,6 +78,130 @@ Grailbird.data.tweets_2019_02 =
 				}
 			},
 			"protected": false,
+			"followers_count": 44368,
+			"friends_count": 2169,
+			"listed_count": 1291,
+			"created_at": "Fri Feb 13 21:27:37 +0000 2009",
+			"favourites_count": 25307,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 169510,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/599827131451998208/W6vlW3NI_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/599827131451998208/W6vlW3NI_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/20808463/1442403377",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 43,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "anybody interested in a #rant on the #earnings of #freelance #journalism? if so, let me first brew some coffee and you can fasten your seat belt."
+	},
+	{
+		"created_at": "Tue Feb 05 11:51:40 +0000 2019",
+		"id": 1092752614285078500,
+		"id_str": "1092752614285078531",
+		"full_text": "anybody interested in a #rant on the #earnings of #freelance #journalism? if so, let me first brew some coffee and you can fasten your seat belt.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			145
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "rant",
+					"indices": [
+						24,
+						29
+					]
+				},
+				{
+					"text": "earnings",
+					"indices": [
+						37,
+						46
+					]
+				},
+				{
+					"text": "freelance",
+					"indices": [
+						50,
+						60
+					]
+				},
+				{
+					"text": "journalism",
+					"indices": [
+						61,
+						72
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 20808463,
+			"id_str": "20808463",
+			"name": "Frederike Geerdink",
+			"screen_name": "fgeerdink",
+			"location": "Suleymanya, Kurdistan",
+			"description": "Journalist in Kurdistan. Spent a year with the PKK and just published the book (Dutch, translations upcoming) about it. Expelled from Turkey, having no regrets.",
+			"url": "https://t.co/5b4PBWJ2R0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/5b4PBWJ2R0",
+							"expanded_url": "https://frederikegeerdink.com/my-books/new-book-the-pkk-from-the-inside/",
+							"display_url": "frederikegeerdink.com/my-books/new-b…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
 			"followers_count": 44360,
 			"friends_count": 2170,
 			"listed_count": 1290,
