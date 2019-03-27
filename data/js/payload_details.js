530c0dc7a2237c5c080478611995610eb81c8bf6
emiracle <ekaymakoglu@gmail.com>
2019-03-27 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5974,
-	"created_at": "Mon Mar 25 01:16:57 +0000 2019",
+	"tweets": 5975,
+	"created_at": "Wed Mar 27 01:16:55 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Tue Mar 26 11:33:08 +0000 2019",
+		"id": 1110504954530275300,
+		"id_str": "1110504954530275329",
+		"full_text": "@sarphanuzunoglu Bu kadar lafa ne gerek var? Sonuçta bunlar orta doğulu değil mi, ne bekliyorsun anlamadım? Norveç mi burası? Burada en basit çiftçiden en \"okumuş\" akademisyene kadar herkes cahillik ve kibir parantezindedir.",
+		"truncated": false,
+		"display_text_range": [
+			17,
+			224
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "sarphanuzunoglu",
+					"name": "Sarphan Uzunoğlu",
+					"id": 34636405,
+					"id_str": "34636405",
+					"indices": [
+						0,
+						16
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1110503228557410300,
+		"in_reply_to_status_id_str": "1110503228557410308",
+		"in_reply_to_user_id": 34636405,
+		"in_reply_to_user_id_str": "34636405",
+		"in_reply_to_screen_name": "sarphanuzunoglu",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 290,
+			"friends_count": 243,
+			"listed_count": 9,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 762,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4728,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@sarphanuzunoglu Bu kadar lafa ne gerek var? Sonuçta bunlar orta doğulu değil mi, ne bekliyorsun anlamadım? Norveç mi burası? Burada en basit çiftçiden en \"okumuş\" akademisyene kadar herkes cahillik ve kibir parantezindedir."
+	},
+	{
 		"created_at": "Sun Mar 24 07:32:40 +0000 2019",
 		"id": 1109719667357167600,
 		"id_str": "1109719667357167616",
