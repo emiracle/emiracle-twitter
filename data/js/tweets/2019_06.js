23c0b11ef25d349295d6fcaecefd8512032d994e
emiracle <ekaymakoglu@gmail.com>
2019-06-03 04:16:56 +0300

    Added 1 tweets

---
+++ data/js/tweets/2019_06.js
@@ -0,0 +1,184 @@
+Grailbird.data.tweets_2019_06 = 
+[
+	{
+		"created_at": "Sun Jun 02 10:09:29 +0000 2019",
+		"id": 1135126281534812200,
+		"id_str": "1135126281534812160",
+		"full_text": "Bizi rezil bir geleceğin kölesi yapmak isteyenler kaybetmeye mahkumdur. https://t.co/tlmJvWQKDa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			71
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/tlmJvWQKDa",
+					"expanded_url": "https://twitter.com/muratgoce/status/1134746046985310208",
+					"display_url": "twitter.com/muratgoce/stat…",
+					"indices": [
+						72,
+						95
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.samruston.co.uk\" rel=\"nofollow\">Flamingo for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 295,
+			"friends_count": 246,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 809,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4787,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1134746046985310200,
+		"quoted_status_id_str": "1134746046985310208",
+		"quoted_status_permalink": {
+			"url": "https://t.co/tlmJvWQKDa",
+			"expanded": "https://twitter.com/muratgoce/status/1134746046985310208",
+			"display": "twitter.com/muratgoce/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Jun 01 08:58:34 +0000 2019",
+			"id": 1134746046985310200,
+			"id_str": "1134746046985310208",
+			"full_text": "Uber yok, booking yok, en önemlisi wikipedia yok ama dijital dönüşüm var, hahaha.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				81
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 34270006,
+				"id_str": "34270006",
+				"name": "Murat Göçe",
+				"screen_name": "muratgoce",
+				"location": "Istanbul",
+				"description": "BThaber , Bilişim Zirvesi , Bilişim 500 ........ \nTEK DERDİM HAYVANLAR VE DOĞA",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2244,
+				"friends_count": 213,
+				"listed_count": 30,
+				"created_at": "Wed Apr 22 13:26:28 +0000 2009",
+				"favourites_count": 12672,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 11833,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1125819468444262402/GSFCwJtj_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1125819468444262402/GSFCwJtj_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/34270006/1517144553",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 321,
+			"favorite_count": 1933,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "tr"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "Bizi rezil bir geleceğin kölesi yapmak isteyenler kaybetmeye mahkumdur. https://t.co/tlmJvWQKDa"
+	}
+]
