c7ce5733470ff59dc474550ab52f8ab5e430ff44
emiracle <ekaymakoglu@gmail.com>
2019-06-04 04:16:58 +0300

    Added 4 tweets

--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,510 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Mon Jun 03 19:40:42 +0000 2019",
+		"id": 1135632417534988300,
+		"id_str": "1135632417534988288",
+		"full_text": "@_mfk @Gamsizm Öyle mi acaba? https://t.co/OnRsgunIpl",
+		"truncated": false,
+		"display_text_range": [
+			15,
+			53
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "_mfk",
+					"name": "mfk",
+					"id": 116770959,
+					"id_str": "116770959",
+					"indices": [
+						0,
+						5
+					]
+				},
+				{
+					"screen_name": "Gamsizm",
+					"name": "Murat Gamsız",
+					"id": 41779319,
+					"id_str": "41779319",
+					"indices": [
+						6,
+						14
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/OnRsgunIpl",
+					"expanded_url": "https://birartibir.org/a-dan-x-e/333-sevgili-tiryaki",
+					"display_url": "birartibir.org/a-dan-x-e/333-…",
+					"indices": [
+						30,
+						53
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.samruston.co.uk\" rel=\"nofollow\">Flamingo for Android</a>",
+		"in_reply_to_status_id": 1135626789445939200,
+		"in_reply_to_status_id_str": "1135626789445939200",
+		"in_reply_to_user_id": 116770959,
+		"in_reply_to_user_id_str": "116770959",
+		"in_reply_to_screen_name": "_mfk",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 294,
+			"friends_count": 246,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 810,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4792,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "@_mfk @Gamsizm Öyle mi acaba? https://t.co/OnRsgunIpl"
+	},
+	{
+		"created_at": "Mon Jun 03 19:33:55 +0000 2019",
+		"id": 1135630711749259300,
+		"id_str": "1135630711749259264",
+		"full_text": "@r_kaan @Gamsizm Murat Gamsız'a sordum ben, seni neden ilgilendiriyor?",
+		"truncated": false,
+		"display_text_range": [
+			17,
+			70
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "r_kaan",
+					"name": "Kaan",
+					"id": 284166686,
+					"id_str": "284166686",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"screen_name": "Gamsizm",
+					"name": "Murat Gamsız",
+					"id": 41779319,
+					"id_str": "41779319",
+					"indices": [
+						8,
+						16
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://www.samruston.co.uk\" rel=\"nofollow\">Flamingo for Android</a>",
+		"in_reply_to_status_id": 1135626960653230100,
+		"in_reply_to_status_id_str": "1135626960653230082",
+		"in_reply_to_user_id": 284166686,
+		"in_reply_to_user_id_str": "284166686",
+		"in_reply_to_screen_name": "r_kaan",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 294,
+			"friends_count": 246,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 810,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4792,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@r_kaan @Gamsizm Murat Gamsız'a sordum ben, seni neden ilgilendiriyor?"
+	},
+	{
+		"created_at": "Mon Jun 03 19:22:57 +0000 2019",
+		"id": 1135627951234584600,
+		"id_str": "1135627951234584576",
+		"full_text": "@ufukbzkurt @Gamsizm \"Daha kötü\" göreceli bir şey. Şeker ve sigara tüketenler illa daha kötü bir hayat sürmek zorunda değil. Kalıtımsal yani gen ile taşınan sistematik hastalıklar daha büyük problem yaratmıyor mu?",
+		"truncated": false,
+		"display_text_range": [
+			21,
+			213
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ufukbzkurt",
+					"name": "Royer",
+					"id": 876025652,
+					"id_str": "876025652",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "Gamsizm",
+					"name": "Murat Gamsız",
+					"id": 41779319,
+					"id_str": "41779319",
+					"indices": [
+						12,
+						20
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://www.samruston.co.uk\" rel=\"nofollow\">Flamingo for Android</a>",
+		"in_reply_to_status_id": 1135607738623549400,
+		"in_reply_to_status_id_str": "1135607738623549441",
+		"in_reply_to_user_id": 876025652,
+		"in_reply_to_user_id_str": "876025652",
+		"in_reply_to_screen_name": "ufukbzkurt",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 294,
+			"friends_count": 246,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 810,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4792,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@ufukbzkurt @Gamsizm \"Daha kötü\" göreceli bir şey. Şeker ve sigara tüketenler illa daha kötü bir hayat sürmek zorunda değil. Kalıtımsal yani gen ile taşınan sistematik hastalıklar daha büyük problem yaratmıyor mu?"
+	},
+	{
+		"created_at": "Mon Jun 03 17:01:01 +0000 2019",
+		"id": 1135570513563193300,
+		"id_str": "1135592232713670657",
+		"full_text": "For most of the world, \"Tank Man\" is the iconic image of the Chinese people standing up for democracy in Tiananmen Square before the government crushed their hopes. But in China, the government has done all it can to wipe out memories of this slaughter. https://t.co/2U0qwCXz5I https://t.co/UHyCwppoCW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2U0qwCXz5I",
+					"expanded_url": "https://trib.al/Ogg1cSL",
+					"display_url": "trib.al/Ogg1cSL",
+					"indices": [
+						254,
+						277
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1135570511780560900,
+					"id_str": "1135570511780560896",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/D8JbXSwWwAAn2EF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8JbXSwWwAAn2EF.jpg",
+					"url": "https://t.co/UHyCwppoCW",
+					"display_url": "pic.twitter.com/UHyCwppoCW",
+					"expanded_url": "https://twitter.com/KenRoth/status/1135570513563193344/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 940,
+							"h": 530,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 940,
+							"h": 530,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1135570511780560900,
+					"id_str": "1135570511780560896",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/D8JbXSwWwAAn2EF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8JbXSwWwAAn2EF.jpg",
+					"url": "https://t.co/UHyCwppoCW",
+					"display_url": "pic.twitter.com/UHyCwppoCW",
+					"expanded_url": "https://twitter.com/KenRoth/status/1135570513563193344/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 940,
+							"h": 530,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 940,
+							"h": 530,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://www.socialflow.com\" rel=\"nofollow\">SocialFlow</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 17839398,
+			"id_str": "17839398",
+			"name": "Kenneth Roth",
+			"screen_name": "KenRoth",
+			"location": "New York",
+			"description": "Executive Director,  \nHuman Rights Watch @HRW",
+			"url": "http://t.co/qNGRw7kDhZ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/qNGRw7kDhZ",
+							"expanded_url": "http://www.hrw.org",
+							"display_url": "hrw.org",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 281476,
+			"friends_count": 951,
+			"listed_count": 5060,
+			"created_at": "Wed Dec 03 15:55:20 +0000 2008",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 63993,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/759478721808068608/F5s3fgyl_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/759478721808068608/F5s3fgyl_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/17839398/1399219549",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 44,
+		"favorite_count": 78,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "For most of the world, \"Tank Man\" is the iconic image of the Chinese people standing up for democracy in Tiananmen Square before the government crushed their hopes. But in China, the government has done all it can to wipe out memories of this slaughter. https://t.co/2U0qwCXz5I https://t.co/UHyCwppoCW"
+	},
+	{
 		"created_at": "Sun Jun 02 10:09:29 +0000 2019",
 		"id": 1135126281534812200,
 		"id_str": "1135126281534812160",
