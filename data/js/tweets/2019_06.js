555bf07ee7a299b7d69a1aca830711c047b3b06b
emiracle <ekaymakoglu@gmail.com>
2019-06-15 04:16:56 +0300

    Added 1 tweets

--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,110 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Sat Jun 15 00:08:57 +0000 2019",
+		"id": 1139686193694556200,
+		"id_str": "1139686193694556162",
+		"full_text": "@tevfik_uyar 80'lerin sonunda bende yurt dışından gelme He-Man sesli hikaye kasetleri vardı. Link'te bir örneğini paylaşıyorum. Türkiye'de ise böyle bir şirket olduğunu zannetmiyorum. https://t.co/tSRKif7W8E",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			207
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "tevfik_uyar",
+					"name": "Tevfik Uyar",
+					"id": 95736749,
+					"id_str": "95736749",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/tSRKif7W8E",
+					"expanded_url": "https://youtu.be/4LhLm6VhizE",
+					"display_url": "youtu.be/4LhLm6VhizE",
+					"indices": [
+						184,
+						207
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.samruston.co.uk\" rel=\"nofollow\">Flamingo for Android</a>",
+		"in_reply_to_status_id": 1139683721982779400,
+		"in_reply_to_status_id_str": "1139683721982779392",
+		"in_reply_to_user_id": 95736749,
+		"in_reply_to_user_id_str": "95736749",
+		"in_reply_to_screen_name": "tevfik_uyar",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 295,
+			"friends_count": 247,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 820,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4810,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "@tevfik_uyar 80'lerin sonunda bende yurt dışından gelme He-Man sesli hikaye kasetleri vardı. Link'te bir örneğini paylaşıyorum. Türkiye'de ise böyle bir şirket olduğunu zannetmiyorum. https://t.co/tSRKif7W8E"
+	},
+	{
 		"created_at": "Thu Jun 13 23:09:54 +0000 2019",
 		"id": 1139308944051101700,
 		"id_str": "1139308944051101701",
