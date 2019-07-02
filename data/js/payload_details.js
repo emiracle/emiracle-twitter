b4a5158b80d77ed8446ed1acb11279a28ca9add0
emiracle <ekaymakoglu@gmail.com>
2019-07-02 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6052,
-	"created_at": "Mon Jul 01 01:16:55 +0000 2019",
+	"tweets": 6053,
+	"created_at": "Tue Jul 02 01:16:55 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_07.js
@@ -0,0 +1,98 @@
+Grailbird.data.tweets_2019_07 = 
+[
+	{
+		"created_at": "Mon Jul 01 14:54:52 +0000 2019",
+		"id": 1145615735475060700,
+		"id_str": "1145707348499030017",
+		"full_text": "After studying science, philosophy, theology and socio economic history for many years, I have concluded that the big problem is that so many people are fucking stupid.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			168
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 20015311,
+			"id_str": "20015311",
+			"name": "Ricky Gervais",
+			"screen_name": "rickygervais",
+			"location": "Earth",
+			"description": "Problematic. Godless Ape. Comedian, Actor, Writer, Director. #TheOffice #Extras #Derek #LifeOnTheRoad #Humanity #DeadlySirius #AfterLife #SuperNature",
+			"url": "https://t.co/SY2wn8O6vt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SY2wn8O6vt",
+							"expanded_url": "http://www.rickygervais.com",
+							"display_url": "rickygervais.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 13314192,
+			"friends_count": 1341,
+			"listed_count": 29392,
+			"created_at": "Wed Feb 04 00:00:12 +0000 2009",
+			"favourites_count": 125608,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 56785,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/642744174744313857/VaPUCm3l_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/642744174744313857/VaPUCm3l_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/20015311/1553371785",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12139,
+		"favorite_count": 76263,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "After studying science, philosophy, theology and socio economic history for many years, I have concluded that the big problem is that so many people are fucking stupid."
+	}
+]
