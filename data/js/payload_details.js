aeeb6b53b69578dd550af8da653a9596c35ff95b
emiracle <ekaymakoglu@gmail.com>
2019-08-07 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6068,
-	"created_at": "Sun Aug 04 01:16:55 +0000 2019",
+	"tweets": 6069,
+	"created_at": "Wed Aug 07 01:16:55 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,109 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Tue Aug 06 09:44:03 +0000 2019",
+		"id": 1158675091720417300,
+		"id_str": "1158675091720417280",
+		"full_text": "@kubimonsta @arrdakadabra Arda doğru söylüyor, sen kendini kandırmaya karar vermişsin, yapacak bir şey yok. Denemesi bedava değil ama :)",
+		"truncated": false,
+		"display_text_range": [
+			26,
+			136
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "kubimonsta",
+					"name": "kubikin skywalker",
+					"id": 86535564,
+					"id_str": "86535564",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "arrdakadabra",
+					"name": "Madame Dôlâ Quai",
+					"id": 604913720,
+					"id_str": "604913720",
+					"indices": [
+						12,
+						25
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://www.samruston.co.uk\" rel=\"nofollow\">Flamingo for Android</a>",
+		"in_reply_to_status_id": 1158615890612629500,
+		"in_reply_to_status_id_str": "1158615890612629505",
+		"in_reply_to_user_id": 86535564,
+		"in_reply_to_user_id_str": "86535564",
+		"in_reply_to_screen_name": "kubimonsta",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 293,
+			"friends_count": 251,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 856,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4850,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@kubimonsta @arrdakadabra Arda doğru söylüyor, sen kendini kandırmaya karar vermişsin, yapacak bir şey yok. Denemesi bedava değil ama :)"
+	},
+	{
 		"created_at": "Sat Aug 03 09:33:28 +0000 2019",
 		"id": 1157296176049348600,
 		"id_str": "1157585264971276288",
