c0a74e586164e540e2963240c84470d56c94aa44
emiracle <ekaymakoglu@gmail.com>
2019-08-17 04:16:56 +0300

    Added 1 tweets

--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,219 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Fri Aug 16 18:05:31 +0000 2019",
+		"id": 1162425166867157000,
+		"id_str": "1162425166867156999",
+		"full_text": "Bu Afili Filintalar tayfası gerçekten de bir virüs. https://t.co/mY2K0k7qZS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			51
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/mY2K0k7qZS",
+					"expanded_url": "https://twitter.com/irmakzileli/status/1154100591083347970",
+					"display_url": "twitter.com/irmakzileli/st…",
+					"indices": [
+						52,
+						75
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 292,
+			"friends_count": 253,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 861,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4856,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1154100591083348000,
+		"quoted_status_id_str": "1154100591083347970",
+		"quoted_status_permalink": {
+			"url": "https://t.co/mY2K0k7qZS",
+			"expanded": "https://twitter.com/irmakzileli/status/1154100591083347970",
+			"display": "twitter.com/irmakzileli/st…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Jul 24 18:46:37 +0000 2019",
+			"id": 1154100591083348000,
+			"id_str": "1154100591083347970",
+			"full_text": "Afili Filintalar'ın edebiyatın başına ördüğü şu çoraptan ne zaman nasıl kurtulacağız merak ediyorum. Özellikle son dönemde okuduğum öykü ve romanlarda dikkatimi çeken bir şey var. Ardı arkası kesilmeyen aforizmalarla yapılan bir \"felsefe\".",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				239
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 145175917,
+				"id_str": "145175917",
+				"name": "Irmak Zileli",
+				"screen_name": "irmakzileli",
+				"location": "",
+				"description": "https://t.co/MwLMS8VwOu https://t.co/UfLGi3uaq9",
+				"url": "https://t.co/5dnZLeGhJs",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/5dnZLeGhJs",
+								"expanded_url": "http://irmakzileli.com.tr",
+								"display_url": "irmakzileli.com.tr",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/MwLMS8VwOu",
+								"expanded_url": "https://www.facebook.com/pages/Irmak-Zileli/1494545654134360?ref=hl",
+								"display_url": "facebook.com/pages/Irmak-Zi…",
+								"indices": [
+									0,
+									23
+								]
+							},
+							{
+								"url": "https://t.co/UfLGi3uaq9",
+								"expanded_url": "http://Instagram.com/irmakzileli",
+								"display_url": "Instagram.com/irmakzileli",
+								"indices": [
+									24,
+									47
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 11309,
+				"friends_count": 1069,
+				"listed_count": 41,
+				"created_at": "Tue May 18 09:02:05 +0000 2010",
+				"favourites_count": 2209,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 5844,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1110561139530838016/PfKVhYLn_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1110561139530838016/PfKVhYLn_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/145175917/1544709717",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 303,
+			"favorite_count": 1912,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "tr"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "Bu Afili Filintalar tayfası gerçekten de bir virüs. https://t.co/mY2K0k7qZS"
+	},
+	{
 		"created_at": "Wed Aug 14 21:58:08 +0000 2019",
 		"id": 1161758930109968400,
 		"id_str": "1161758930109968384",
