fde0a2375e48a6c4722925c9d8fe57708f23192e
emiracle <ekaymakoglu@gmail.com>
2019-09-10 04:16:57 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6082,
-	"created_at": "Sat Sep 07 01:16:55 +0000 2019",
+	"tweets": 6083,
+	"created_at": "Tue Sep 10 01:16:56 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,178 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Mon Sep 09 16:22:44 +0000 2019",
+		"id": 1171096608429236200,
+		"id_str": "1171096608429236225",
+		"full_text": "@imTolstoyevski Rahmetliye sordum, robotik sesiyle \"Siktirsin amk şarlatanları\" dedi. Kesin bilgi, yayalım! https://t.co/7aRQafyWiX",
+		"truncated": false,
+		"display_text_range": [
+			16,
+			107
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "imTolstoyevski",
+					"name": "Immanuel Tolstoyevski",
+					"id": 224816746,
+					"id_str": "224816746",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1171096602427166700,
+					"id_str": "1171096602427166721",
+					"indices": [
+						108,
+						131
+					],
+					"media_url": "http://pbs.twimg.com/media/EECSJ7lWkAE3JrO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EECSJ7lWkAE3JrO.jpg",
+					"url": "https://t.co/7aRQafyWiX",
+					"display_url": "pic.twitter.com/7aRQafyWiX",
+					"expanded_url": "https://twitter.com/emiracle/status/1171096608429236225/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1171096602427166700,
+					"id_str": "1171096602427166721",
+					"indices": [
+						108,
+						131
+					],
+					"media_url": "http://pbs.twimg.com/media/EECSJ7lWkAE3JrO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EECSJ7lWkAE3JrO.jpg",
+					"url": "https://t.co/7aRQafyWiX",
+					"display_url": "pic.twitter.com/7aRQafyWiX",
+					"expanded_url": "https://twitter.com/emiracle/status/1171096608429236225/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://www.samruston.co.uk\" rel=\"nofollow\">Flamingo for Android</a>",
+		"in_reply_to_status_id": 1170961013983850500,
+		"in_reply_to_status_id_str": "1170961013983850496",
+		"in_reply_to_user_id": 224816746,
+		"in_reply_to_user_id_str": "224816746",
+		"in_reply_to_screen_name": "imTolstoyevski",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 292,
+			"friends_count": 255,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 879,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4869,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "@imTolstoyevski Rahmetliye sordum, robotik sesiyle \"Siktirsin amk şarlatanları\" dedi. Kesin bilgi, yayalım! https://t.co/7aRQafyWiX"
+	},
+	{
 		"created_at": "Fri Sep 06 14:57:53 +0000 2019",
 		"id": 1169988091643400200,
 		"id_str": "1169988091643400193",
