9a403eea6117cd12be7864d217dbf41675ba75cd
emiracle <ekaymakoglu@gmail.com>
2019-09-25 04:16:56 +0300

    Added 1 tweets

--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Tue Sep 24 17:52:21 +0000 2019",
+		"id": 1176554980788121600,
+		"id_str": "1176554980788121601",
+		"full_text": "@imTolstoyevski Süresini bilmiyorum ama desibel bayağı arttı. https://t.co/ktlf47Bzco",
+		"truncated": false,
+		"display_text_range": [
+			16,
+			61
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "imTolstoyevski",
+					"name": "Immanuel Tolstoyevski (Fularsız Entellik)",
+					"id": 224816746,
+					"id_str": "224816746",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1176554966909165600,
+					"id_str": "1176554966909165568",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EFP2gj1XoAAHcHR.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EFP2gj1XoAAHcHR.jpg",
+					"url": "https://t.co/ktlf47Bzco",
+					"display_url": "pic.twitter.com/ktlf47Bzco",
+					"expanded_url": "https://twitter.com/emiracle/status/1176554980788121601/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 404,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 404,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1176554966909165600,
+					"id_str": "1176554966909165568",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EFP2gj1XoAAHcHR.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EFP2gj1XoAAHcHR.jpg",
+					"url": "https://t.co/ktlf47Bzco",
+					"display_url": "pic.twitter.com/ktlf47Bzco",
+					"expanded_url": "https://twitter.com/emiracle/status/1176554980788121601/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 404,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 404,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							180,
+							101
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EFP2gj1XoAAHcHR.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1176554418617106400,
+		"in_reply_to_status_id_str": "1176554418617106433",
+		"in_reply_to_user_id": 224816746,
+		"in_reply_to_user_id_str": "224816746",
+		"in_reply_to_screen_name": "imTolstoyevski",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 290,
+			"friends_count": 259,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 890,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4886,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "@imTolstoyevski Süresini bilmiyorum ama desibel bayağı arttı. https://t.co/ktlf47Bzco"
+	},
+	{
 		"created_at": "Mon Sep 23 07:14:39 +0000 2019",
 		"id": 1176032110911590400,
 		"id_str": "1176032110911590400",
