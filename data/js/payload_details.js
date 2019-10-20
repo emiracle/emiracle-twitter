7638380563867b0102020af181d160bb1056a514
emiracle <ekaymakoglu@gmail.com>
2019-10-20 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6118,
-	"created_at": "Sat Oct 19 01:16:55 +0000 2019",
+	"tweets": 6119,
+	"created_at": "Sun Oct 20 01:16:55 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,10 +1,103 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Sat Oct 19 10:38:16 +0000 2019",
+		"id": 1185505436772028400,
+		"id_str": "1185505436772028418",
+		"full_text": "@korayphlvglu Anatole France'ın sözü değil mi bu?",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			49
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "korayphlvglu",
+					"name": "Koray Pehlivanoğlu",
+					"id": 190247958,
+					"id_str": "190247958",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://www.samruston.co.uk\" rel=\"nofollow\">Flamingo for Android</a>",
+		"in_reply_to_status_id": 1185503178202194000,
+		"in_reply_to_status_id_str": "1185503178202193920",
+		"in_reply_to_user_id": 190247958,
+		"in_reply_to_user_id_str": "190247958",
+		"in_reply_to_screen_name": "korayphlvglu",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 290,
+			"friends_count": 260,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 926,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4918,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@korayphlvglu Anatole France'ın sözü değil mi bu?"
+	},
+	{
 		"created_at": "Fri Oct 18 17:51:54 +0000 2019",
 		"id": 1185252176815370200,
 		"id_str": "1185252176815370246",
-		"full_text": "@wardumpp @ilkercanikligil Ben bu tweet'leri görmemiştim. Bilsem  baştan hiç bu topa girmezdim. Şu üsluba şu derinliğe bakar mısın? Eric Cartman, DonanımHaber forumlarında yazıyor sanki. 🤦‍♂️ https://t.co/vqbhm0lwpJ",
+		"full_text": "@wardumpp @ilkercanikligil Ben bu tweet'leri görmemiştim. Bilsem  baştan hiç bu topa girmezdim. Şu üsluba şu derinliğe bakar mısın? Eric Cartman, DonanımHaber forumlarında yazıyor sanki. 🤦♂️ https://t.co/vqbhm0lwpJ",
 		"truncated": false,
 		"display_text_range": [
 			27,
@@ -180,7 +273,7 @@ Grailbird.data.tweets_2019_10 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "tr",
-		"text": "@wardumpp @ilkercanikligil Ben bu tweet'leri görmemiştim. Bilsem  baştan hiç bu topa girmezdim. Şu üsluba şu derinliğe bakar mısın? Eric Cartman, DonanımHaber forumlarında yazıyor sanki. 🤦‍♂️ https://t.co/vqbhm0lwpJ"
+		"text": "@wardumpp @ilkercanikligil Ben bu tweet'leri görmemiştim. Bilsem  baştan hiç bu topa girmezdim. Şu üsluba şu derinliğe bakar mısın? Eric Cartman, DonanımHaber forumlarında yazıyor sanki. 🤦♂️ https://t.co/vqbhm0lwpJ"
 	},
 	{
 		"created_at": "Fri Oct 18 15:34:03 +0000 2019",
