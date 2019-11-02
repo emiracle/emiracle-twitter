8c3a0697d7ceb56fd65c43ed57b147e1ec394d7f
emiracle <ekaymakoglu@gmail.com>
2019-11-02 04:16:57 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6130,
-	"created_at": "Fri Nov 01 01:16:55 +0000 2019",
+	"tweets": 6131,
+	"created_at": "Sat Nov 02 01:16:56 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_11.js
@@ -0,0 +1,118 @@
+Grailbird.data.tweets_2019_11 = 
+[
+	{
+		"created_at": "Fri Nov 01 16:48:59 +0000 2019",
+		"id": 1190309771502592000,
+		"id_str": "1190309771502592003",
+		"full_text": "@bulentbab @leventp @gokhanyucel Ben de öyle yaptım. YouTube Premium'un Spotify'dan eksiği olmadığı gibi fazlası da var.",
+		"truncated": false,
+		"display_text_range": [
+			33,
+			120
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "bulentbab",
+					"name": "rabia naz vatan'ın katilleri hesap versin.",
+					"id": 54224787,
+					"id_str": "54224787",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "leventp",
+					"name": "Levent Pekcan",
+					"id": 755912,
+					"id_str": "755912",
+					"indices": [
+						11,
+						19
+					]
+				},
+				{
+					"screen_name": "gokhanyucel",
+					"name": "gökhan yücel",
+					"id": 18138107,
+					"id_str": "18138107",
+					"indices": [
+						20,
+						32
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://www.samruston.co.uk\" rel=\"nofollow\">Flamingo for Android</a>",
+		"in_reply_to_status_id": 1190219840654577700,
+		"in_reply_to_status_id_str": "1190219840654577665",
+		"in_reply_to_user_id": 54224787,
+		"in_reply_to_user_id_str": "54224787",
+		"in_reply_to_screen_name": "bulentbab",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 289,
+			"friends_count": 260,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 936,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4936,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@bulentbab @leventp @gokhanyucel Ben de öyle yaptım. YouTube Premium'un Spotify'dan eksiği olmadığı gibi fazlası da var."
+	}
+]
