749f224734614e251bd1fc8a0ff8508cb01cdb2a
emiracle <ekaymakoglu@gmail.com>
2019-11-17 04:16:57 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6135,
-	"created_at": "Thu Nov 14 01:16:56 +0000 2019",
+	"tweets": 6136,
+	"created_at": "Sun Nov 17 01:16:56 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,321 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Sat Nov 16 19:37:22 +0000 2019",
+		"id": 1195787964418277400,
+		"id_str": "1195787964418277377",
+		"full_text": "Bu artık ne tür bir sapıklık? Uyduruk bir B-Movie senaryosu mu bu? https://t.co/uewUXwBfzU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			66
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/uewUXwBfzU",
+					"expanded_url": "https://twitter.com/evrenbarisyavuz/status/1195728815546413061",
+					"display_url": "twitter.com/evrenbarisyavu…",
+					"indices": [
+						67,
+						90
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.samruston.co.uk\" rel=\"nofollow\">Flamingo for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 288,
+			"friends_count": 256,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 943,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4943,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1195728815546413000,
+		"quoted_status_id_str": "1195728815546413061",
+		"quoted_status_permalink": {
+			"url": "https://t.co/uewUXwBfzU",
+			"expanded": "https://twitter.com/evrenbarisyavuz/status/1195728815546413061",
+			"display": "twitter.com/evrenbarisyavu…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Nov 16 15:42:19 +0000 2019",
+			"id": 1195728815546413000,
+			"id_str": "1195728815546413061",
+			"full_text": "Göle ne oldu?\n\nGümüşhane'de öylece duran gölü çöl ettiler, üstünü kapatıp gittiler. Bu ülkede olan bitenin en yalın hali şu gölcüğün başına gelenler. https://t.co/8PIopb8g2z",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				149
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1195728796508446700,
+						"id_str": "1195728796508446722",
+						"indices": [
+							150,
+							173
+						],
+						"media_url": "http://pbs.twimg.com/media/EJgVAVOXYAICXc9.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EJgVAVOXYAICXc9.jpg",
+						"url": "https://t.co/8PIopb8g2z",
+						"display_url": "pic.twitter.com/8PIopb8g2z",
+						"expanded_url": "https://twitter.com/evrenbarisyavuz/status/1195728815546413061/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 988,
+								"h": 369,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 988,
+								"h": 369,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 254,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"orig": {
+								"faces": []
+							},
+							"medium": {
+								"faces": []
+							},
+							"large": {
+								"faces": []
+							},
+							"small": {
+								"faces": []
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1195728796508446700,
+						"id_str": "1195728796508446722",
+						"indices": [
+							150,
+							173
+						],
+						"media_url": "http://pbs.twimg.com/media/EJgVAVOXYAICXc9.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EJgVAVOXYAICXc9.jpg",
+						"url": "https://t.co/8PIopb8g2z",
+						"display_url": "pic.twitter.com/8PIopb8g2z",
+						"expanded_url": "https://twitter.com/evrenbarisyavuz/status/1195728815546413061/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 988,
+								"h": 369,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 988,
+								"h": 369,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 254,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"orig": {
+								"faces": []
+							},
+							"medium": {
+								"faces": []
+							},
+							"large": {
+								"faces": []
+							},
+							"small": {
+								"faces": []
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 79176661,
+				"id_str": "79176661",
+				"name": "Evren Barış Yavuz",
+				"screen_name": "evrenbarisyavuz",
+				"location": "Kadıköy, İstanbul",
+				"description": "Anlatıcı  #DTCFtiyatro\n\n📷 https://t.co/Dd86ofxtva\n\n#Antifa",
+				"url": "https://t.co/wp8OsxOai6",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/wp8OsxOai6",
+								"expanded_url": "http://evrenbarisyavuz.com/",
+								"display_url": "evrenbarisyavuz.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/Dd86ofxtva",
+								"expanded_url": "https://vsco.co/evrenbarisyavuz/images/1",
+								"display_url": "vsco.co/evrenbarisyavu…",
+								"indices": [
+									26,
+									49
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 41711,
+				"friends_count": 1122,
+				"listed_count": 157,
+				"created_at": "Fri Oct 02 13:10:48 +0000 2009",
+				"favourites_count": 631,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 8745,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": true,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1187115752551264256/ofsm6ewb_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1187115752551264256/ofsm6ewb_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/79176661/1562314360",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "181A1E",
+				"profile_sidebar_fill_color": "252429",
+				"profile_text_color": "666666",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": false,
+				"followed_by": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 22,
+			"favorite_count": 73,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "tr"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "Bu artık ne tür bir sapıklık? Uyduruk bir B-Movie senaryosu mu bu? https://t.co/uewUXwBfzU"
+	},
+	{
 		"created_at": "Wed Nov 13 20:27:28 +0000 2019",
 		"id": 1194713409687871500,
 		"id_str": "1194713409687871489",
