4dbd6c3b0cf8f64bca6b4071dde9f64e1aa1c190
emiracle <ekaymakoglu@gmail.com>
2019-11-26 04:16:57 +0300

    Added 1 tweets

--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,111 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Mon Nov 25 12:37:51 +0000 2019",
+		"id": 1198943884354740200,
+		"id_str": "1198943884354740225",
+		"full_text": "@AkselFenerci @imTolstoyevski Çok haklısın. Öğretmenler milli ideoloji ile soslu vasat öğrenciler yetiştirmekle mükellef vasat memurlardır. Gerçekten değerli bir öğretmen varsa da devlet hemen gereğini yapar, sistemden çıkartır hemen.",
+		"truncated": false,
+		"display_text_range": [
+			30,
+			234
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "AkselFenerci",
+					"name": "Aksel Fenerci",
+					"id": 1496188854,
+					"id_str": "1496188854",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "imTolstoyevski",
+					"name": "Immanuel Tolstoyevski (Fularsız Entellik)",
+					"id": 224816746,
+					"id_str": "224816746",
+					"indices": [
+						14,
+						29
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://www.samruston.co.uk\" rel=\"nofollow\">Flamingo for Android</a>",
+		"in_reply_to_status_id": 1198923259967873000,
+		"in_reply_to_status_id_str": "1198923259967873024",
+		"in_reply_to_user_id": 1496188854,
+		"in_reply_to_user_id_str": "1496188854",
+		"in_reply_to_screen_name": "AkselFenerci",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 288,
+			"friends_count": 256,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 947,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4948,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@AkselFenerci @imTolstoyevski Çok haklısın. Öğretmenler milli ideoloji ile soslu vasat öğrenciler yetiştirmekle mükellef vasat memurlardır. Gerçekten değerli bir öğretmen varsa da devlet hemen gereğini yapar, sistemden çıkartır hemen."
+	},
+	{
 		"created_at": "Sun Nov 24 14:40:51 +0000 2019",
 		"id": 1198612446983999500,
 		"id_str": "1198612446983999488",
