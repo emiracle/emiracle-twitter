d635e91c7d994fa6e62db76ed74d114c5fab1c41
emiracle <ekaymakoglu@gmail.com>
2019-12-03 04:16:56 +0300

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6143,
-	"created_at": "Thu Nov 28 01:16:55 +0000 2019",
+	"tweets": 6145,
+	"created_at": "Tue Dec 03 01:16:55 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_12.js
@@ -0,0 +1,345 @@
+Grailbird.data.tweets_2019_12 = 
+[
+	{
+		"created_at": "Mon Dec 02 18:04:02 +0000 2019",
+		"id": 1201562683667038200,
+		"id_str": "1201562683667038209",
+		"full_text": "@arrdakadabra Plus. Ne fark ediyor? 🤓",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			37
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "arrdakadabra",
+					"name": "Madame Dôlâ Quai",
+					"id": 604913720,
+					"id_str": "604913720",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1201561651801145300,
+		"in_reply_to_status_id_str": "1201561651801145345",
+		"in_reply_to_user_id": 604913720,
+		"in_reply_to_user_id_str": "604913720",
+		"in_reply_to_screen_name": "arrdakadabra",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 288,
+			"friends_count": 256,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 953,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4952,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@arrdakadabra Plus. Ne fark ediyor? 🤓"
+	},
+	{
+		"created_at": "Mon Dec 02 17:00:39 +0000 2019",
+		"id": 1201546734096330800,
+		"id_str": "1201546734096330752",
+		"full_text": "Bu da başka bir AI'ın gelmiş olduğu seviyeyi gösteriyor. Eğer doğru düzgün yazarsanız, Google Translate mükemmel çeviriyor. Makine öğrenmesi dediniz, öğrendi sonunda. https://t.co/Sx2i6ZWwgj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			166
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1201546727049891800,
+					"id_str": "1201546727049891840",
+					"indices": [
+						167,
+						190
+					],
+					"media_url": "http://pbs.twimg.com/media/EKzAYdbXkAADV4a.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKzAYdbXkAADV4a.jpg",
+					"url": "https://t.co/Sx2i6ZWwgj",
+					"display_url": "pic.twitter.com/Sx2i6ZWwgj",
+					"expanded_url": "https://twitter.com/emiracle/status/1201546734096330752/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 363,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1196,
+							"h": 638,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1196,
+							"h": 638,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 28,
+									"y": 28,
+									"h": 28,
+									"w": 28
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 51,
+									"y": 50,
+									"h": 50,
+									"w": 50
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 51,
+									"y": 50,
+									"h": 50,
+									"w": 50
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 51,
+									"y": 50,
+									"h": 50,
+									"w": 50
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1201546727049891800,
+					"id_str": "1201546727049891840",
+					"indices": [
+						167,
+						190
+					],
+					"media_url": "http://pbs.twimg.com/media/EKzAYdbXkAADV4a.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKzAYdbXkAADV4a.jpg",
+					"url": "https://t.co/Sx2i6ZWwgj",
+					"display_url": "pic.twitter.com/Sx2i6ZWwgj",
+					"expanded_url": "https://twitter.com/emiracle/status/1201546734096330752/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 363,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1196,
+							"h": 638,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1196,
+							"h": 638,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 28,
+									"y": 28,
+									"h": 28,
+									"w": 28
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 51,
+									"y": 50,
+									"h": 50,
+									"w": 50
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 51,
+									"y": 50,
+									"h": 50,
+									"w": 50
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 51,
+									"y": 50,
+									"h": 50,
+									"w": 50
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1201545868295463000,
+		"in_reply_to_status_id_str": "1201545868295462918",
+		"in_reply_to_user_id": 18937325,
+		"in_reply_to_user_id_str": "18937325",
+		"in_reply_to_screen_name": "emiracle",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 288,
+			"friends_count": 256,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 953,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4952,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "Bu da başka bir AI'ın gelmiş olduğu seviyeyi gösteriyor. Eğer doğru düzgün yazarsanız, Google Translate mükemmel çeviriyor. Makine öğrenmesi dediniz, öğrendi sonunda. https://t.co/Sx2i6ZWwgj"
+	}
+]
