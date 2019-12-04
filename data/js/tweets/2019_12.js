27ecdf2064db94490481151eba3f455fe00f1ca5
emiracle <ekaymakoglu@gmail.com>
2019-12-04 04:16:56 +0300

    Added 1 tweets

--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,109 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Tue Dec 03 10:02:05 +0000 2019",
+		"id": 1201803785183662000,
+		"id_str": "1201803785183662082",
+		"full_text": "@PlayStationTR Metal Gear Solid'te Psycho Mantis'i yenmek için DualShock port'unu fiziksel olarak değiştirme gereksinimi, bir video oyununda bugüne kadar gördüğüm en iyi mekaniklerinden biriydi. #25YearsOfPlay oldu unutamadım!",
+		"truncated": false,
+		"display_text_range": [
+			15,
+			226
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "25YearsOfPlay",
+					"indices": [
+						195,
+						209
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "PlayStationTR",
+					"name": "PlayStation Türkiye",
+					"id": 229763989,
+					"id_str": "229763989",
+					"indices": [
+						0,
+						14
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1201801455918821400,
+		"in_reply_to_status_id_str": "1201801455918821376",
+		"in_reply_to_user_id": 229763989,
+		"in_reply_to_user_id_str": "229763989",
+		"in_reply_to_screen_name": "PlayStationTR",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 289,
+			"friends_count": 256,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 954,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4953,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@PlayStationTR Metal Gear Solid'te Psycho Mantis'i yenmek için DualShock port'unu fiziksel olarak değiştirme gereksinimi, bir video oyununda bugüne kadar gördüğüm en iyi mekaniklerinden biriydi. #25YearsOfPlay oldu unutamadım!"
+	},
+	{
 		"created_at": "Mon Dec 02 18:04:02 +0000 2019",
 		"id": 1201562683667038200,
 		"id_str": "1201562683667038209",
