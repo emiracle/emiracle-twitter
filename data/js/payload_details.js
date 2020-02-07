72fe4ddd529c82f6bf6cd6ab9b6540457faf7bda
emiracle <ekaymakoglu@gmail.com>
2020-02-07 04:16:58 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6197,
-	"created_at": "Thu Feb 06 01:16:56 +0000 2020",
+	"tweets": 6198,
+	"created_at": "Fri Feb 07 01:16:56 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,175 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Thu Feb 06 21:40:35 +0000 2020",
+		"id": 1225534780047077400,
+		"id_str": "1225534780047077376",
+		"full_text": "#GeziDavası diye bir dava yok. O ancak sizin*ikilmiş g*tünüzün davasıdır ve herkes bilir ki *ikilmiş g*tün davası olmaz. O ağaçlar orada durmaya devam edecek ama siz siktir olup gideceksiniz. https://t.co/nSAXYBqPOi",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			191
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GeziDavası",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1225534768869191700,
+					"id_str": "1225534768869191681",
+					"indices": [
+						192,
+						215
+					],
+					"media_url": "http://pbs.twimg.com/media/EQH5YQ5WsAEYGL9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQH5YQ5WsAEYGL9.jpg",
+					"url": "https://t.co/nSAXYBqPOi",
+					"display_url": "pic.twitter.com/nSAXYBqPOi",
+					"expanded_url": "https://twitter.com/emiracle/status/1225534780047077376/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1225534768869191700,
+					"id_str": "1225534768869191681",
+					"indices": [
+						192,
+						215
+					],
+					"media_url": "http://pbs.twimg.com/media/EQH5YQ5WsAEYGL9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQH5YQ5WsAEYGL9.jpg",
+					"url": "https://t.co/nSAXYBqPOi",
+					"display_url": "pic.twitter.com/nSAXYBqPOi",
+					"expanded_url": "https://twitter.com/emiracle/status/1225534780047077376/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 292,
+			"friends_count": 259,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1014,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5027,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "#GeziDavası diye bir dava yok. O ancak sizin*ikilmiş g*tünüzün davasıdır ve herkes bilir ki *ikilmiş g*tün davası olmaz. O ağaçlar orada durmaya devam edecek ama siz siktir olup gideceksiniz. https://t.co/nSAXYBqPOi"
+	},
+	{
 		"created_at": "Wed Feb 05 21:23:33 +0000 2020",
 		"id": 1225168105313316900,
 		"id_str": "1225168105313316865",
