20a44d6fb96eec14d7e674fed21e4fd5e1e29e1e
emiracle <ekaymakoglu@gmail.com>
2020-03-17 04:16:58 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6260,
-	"created_at": "Mon Mar 16 01:16:56 +0000 2020",
+	"tweets": 6261,
+	"created_at": "Tue Mar 17 01:16:57 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,119 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Mon Mar 16 10:39:23 +0000 2020",
+		"id": 1239501511077638100,
+		"id_str": "1239501511077638144",
+		"full_text": "@ozgurmumcu @imTolstoyevski @ErayOzer Ben tarafsız olarak söylüyorum: İkinizin ki de çok iyi, tebrikler! 😉",
+		"truncated": false,
+		"display_text_range": [
+			38,
+			106
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ozgurmumcu",
+					"name": "Özgür Mumcu",
+					"id": 34609354,
+					"id_str": "34609354",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "imTolstoyevski",
+					"name": "Immanuel Tolstoyevski (Fularsız Entellik)",
+					"id": 224816746,
+					"id_str": "224816746",
+					"indices": [
+						12,
+						27
+					]
+				},
+				{
+					"screen_name": "ErayOzer",
+					"name": "Eray Özer",
+					"id": 68681919,
+					"id_str": "68681919",
+					"indices": [
+						28,
+						37
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1239501143765590000,
+		"in_reply_to_status_id_str": "1239501143765590016",
+		"in_reply_to_user_id": 34609354,
+		"in_reply_to_user_id_str": "34609354",
+		"in_reply_to_screen_name": "ozgurmumcu",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 292,
+			"friends_count": 262,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1056,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5105,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@ozgurmumcu @imTolstoyevski @ErayOzer Ben tarafsız olarak söylüyorum: İkinizin ki de çok iyi, tebrikler! 😉"
+	},
+	{
 		"created_at": "Sun Mar 15 17:10:09 +0000 2020",
 		"id": 1239237463744417800,
 		"id_str": "1239237463744417792",
