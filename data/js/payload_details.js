4809bcfc8168c8b6584de4d88f49c24b5dfe9eff
emiracle <ekaymakoglu@gmail.com>
2020-04-02 04:16:57 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6297,
-	"created_at": "Wed Apr 01 01:16:56 +0000 2020",
+	"tweets": 6298,
+	"created_at": "Thu Apr 02 01:16:56 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_04.js
@@ -0,0 +1,131 @@
+Grailbird.data.tweets_2020_04 = 
+[
+	{
+		"created_at": "Wed Apr 01 23:50:32 +0000 2020",
+		"id": 1244349322906480600,
+		"id_str": "1245498817413332993",
+		"full_text": "For those who wanted a world with no vaccines... here’s the world without ONE vaccine.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			86
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 38256988,
+			"id_str": "38256988",
+			"name": "Carlos A. Rodríguez",
+			"screen_name": "CarlosHappyNPO",
+			"location": "Puerto Rico",
+			"description": "Father of 3. Married a Brit. Puertorriqueño + director of the happy non-profit.",
+			"url": "https://t.co/jnWNdPf9JM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jnWNdPf9JM",
+							"expanded_url": "http://HappyNPO.com",
+							"display_url": "HappyNPO.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 35240,
+			"friends_count": 866,
+			"listed_count": 279,
+			"created_at": "Wed May 06 19:33:46 +0000 2009",
+			"favourites_count": 56274,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18784,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/932300917776568320/A15E0z6Q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/932300917776568320/A15E0z6Q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/38256988/1499125002",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "f98a7b4c97c21162",
+			"url": "https://api.twitter.com/1.1/geo/id/f98a7b4c97c21162.json",
+			"place_type": "city",
+			"name": "Breñas",
+			"full_name": "Breñas, USA",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-66.344533,
+							18.465153
+						],
+						[
+							-66.325941,
+							18.465153
+						],
+						[
+							-66.325941,
+							18.481666
+						],
+						[
+							-66.344533,
+							18.481666
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 25485,
+		"favorite_count": 61384,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "For those who wanted a world with no vaccines... here’s the world without ONE vaccine."
+	}
+]
