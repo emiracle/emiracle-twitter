055710f79e63c6e5c71db24597d4fa481a451932
emiracle <ekaymakoglu@gmail.com>
2020-04-03 04:16:56 +0300

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6298,
-	"created_at": "Thu Apr 02 01:16:56 +0000 2020",
+	"tweets": 6300,
+	"created_at": "Fri Apr 03 01:16:55 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,426 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Thu Apr 02 22:39:29 +0000 2020",
+		"id": 1245826243338989600,
+		"id_str": "1245843321605558273",
+		"full_text": "Kediler yürürken daha az iz bırakmak, daha sessiz yürüyebilmek ve daha stabil adım atmak için arka ayakları ile ön ayaklarının bastığı yere basarak yürüyorlarmış. https://t.co/08wNaFoAH3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			162
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/08wNaFoAH3",
+					"expanded_url": "https://twitter.com/hanamomoact/status/618663329200910336",
+					"display_url": "twitter.com/hanamomoact/st…",
+					"indices": [
+						163,
+						186
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 20569249,
+			"id_str": "20569249",
+			"name": "Işıl Arıcan 🚿👋🧼🚿",
+			"screen_name": "isil_arican",
+			"location": "San Francisco Bay Area",
+			"description": "Skeptic • Geek • Medical Doctor • Healthcare IT Exec • Science Writer • Founder of @yalansavar & @trepanasyon • Beer Brewer • RPG Player • Cat Slave • ENG & TR",
+			"url": "https://t.co/hGXN4sR411",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hGXN4sR411",
+							"expanded_url": "http://yalansavar.org",
+							"display_url": "yalansavar.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 32475,
+			"friends_count": 995,
+			"listed_count": 208,
+			"created_at": "Wed Feb 11 03:45:11 +0000 2009",
+			"favourites_count": 23078,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 29812,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1190320563505549312/THhPJonQ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1190320563505549312/THhPJonQ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/20569249/1471928597",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 618663329200910300,
+		"quoted_status_id_str": "618663329200910336",
+		"quoted_status_permalink": {
+			"url": "https://t.co/08wNaFoAH3",
+			"expanded": "https://twitter.com/hanamomoact/status/618663329200910336",
+			"display": "twitter.com/hanamomoact/st…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Jul 08 06:10:06 +0000 2015",
+			"id": 618663329200910300,
+			"id_str": "618663329200910336",
+			"full_text": "こちらが、足跡が一直線になる過程。 http://t.co/OFsTfoUqTc",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				40
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 618662591439614000,
+						"id_str": "618662591439613954",
+						"indices": [
+							18,
+							40
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/618662591439613954/pu/img/AhjRwTHBiQ1ksi6b.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/618662591439613954/pu/img/AhjRwTHBiQ1ksi6b.jpg",
+						"url": "http://t.co/OFsTfoUqTc",
+						"display_url": "pic.twitter.com/OFsTfoUqTc",
+						"expanded_url": "https://twitter.com/hanamomoact/status/618663329200910336/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 618662591439614000,
+						"id_str": "618662591439613954",
+						"indices": [
+							18,
+							40
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/618662591439613954/pu/img/AhjRwTHBiQ1ksi6b.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/618662591439613954/pu/img/AhjRwTHBiQ1ksi6b.jpg",
+						"url": "http://t.co/OFsTfoUqTc",
+						"display_url": "pic.twitter.com/OFsTfoUqTc",
+						"expanded_url": "https://twitter.com/hanamomoact/status/618663329200910336/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 30000,
+							"variants": [
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/618662591439613954/pu/pl/J1gBzKNUuNrux2KJ.m3u8"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/618662591439613954/pu/vid/1280x720/fDA-pVLN_yBcxkkn.mp4"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/618662591439613954/pu/vid/640x360/8KgB-b3_vp__uld_.mp4"
+								},
+								{
+									"bitrate": 320000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/618662591439613954/pu/vid/320x180/fpZCcTdf1XHTVzWO.mp4"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 264277107,
+				"id_str": "264277107",
+				"name": "はなもも",
+				"screen_name": "hanamomoact",
+				"location": "UAE",
+				"description": "UAEのAl-Ainの砂漠でガゼルとか鳩とか犬とか馬とか兎とかラクダとか山羊とか羊とか牛とかニワトリとか猫とかと生活してます。こんな生活になった経緯や今を紹介した写真集「砂漠のわが家」「Life in the Desert 砂漠に棲む」が出ています。https://t.co/yicNo6mbRa",
+				"url": "https://t.co/EPWHSX8r7s",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/EPWHSX8r7s",
+								"expanded_url": "http://www.youtube.com/c/HanamomoWhat'snewtoday",
+								"display_url": "youtube.com/c/HanamomoWhat…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/yicNo6mbRa",
+								"expanded_url": "http://hanamomoac.exblog.jp/",
+								"display_url": "hanamomoac.exblog.jp",
+								"indices": [
+									125,
+									148
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 116113,
+				"friends_count": 126,
+				"listed_count": 2766,
+				"created_at": "Fri Mar 11 16:47:58 +0000 2011",
+				"favourites_count": 2046,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 35386,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "B2DFDA",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/472471111880294400/mTcSuR7s_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/472471111880294400/mTcSuR7s_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/264277107/1401430106",
+				"profile_link_color": "93A644",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "FFFFFF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 20381,
+			"favorite_count": 27906,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "ja"
+		},
+		"retweet_count": 33,
+		"favorite_count": 480,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "Kediler yürürken daha az iz bırakmak, daha sessiz yürüyebilmek ve daha stabil adım atmak için arka ayakları ile ön ayaklarının bastığı yere basarak yürüyorlarmış. https://t.co/08wNaFoAH3"
+	},
+	{
+		"created_at": "Thu Apr 02 22:37:26 +0000 2020",
+		"id": 1245842808004661200,
+		"id_str": "1245842808004661260",
+		"full_text": "@samettulca Great tune! It's really better with headphones. Congratz 👍🏻👏🏻",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			73
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "samettulca",
+					"name": "Samet Tulça",
+					"id": 962355935258824700,
+					"id_str": "962355935258824704",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1245751107139354600,
+		"in_reply_to_status_id_str": "1245751107139354624",
+		"in_reply_to_user_id": 962355935258824700,
+		"in_reply_to_user_id_str": "962355935258824704",
+		"in_reply_to_screen_name": "samettulca",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 294,
+			"friends_count": 265,
+			"listed_count": 9,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1113,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5157,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@samettulca Great tune! It's really better with headphones. Congratz 👍🏻👏🏻"
+	},
+	{
 		"created_at": "Wed Apr 01 23:50:32 +0000 2020",
 		"id": 1244349322906480600,
 		"id_str": "1245498817413332993",
