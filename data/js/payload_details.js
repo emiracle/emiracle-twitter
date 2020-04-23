fb2fee6eaab38501f830ca0fe8f220612532c3c4
emiracle <ekaymakoglu@gmail.com>
2020-04-23 04:16:57 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6352,
-	"created_at": "Wed Apr 22 01:16:56 +0000 2020",
+	"tweets": 6353,
+	"created_at": "Thu Apr 23 01:16:56 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,109 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Wed Apr 22 13:51:33 +0000 2020",
+		"id": 1252958220932563000,
+		"id_str": "1252958220932562945",
+		"full_text": "@DikenComTr @umit_k Aynı haberi Çin geçen yerleri Türkiye ile değiştirerek de okuyabilirsiniz. Çin'in tek farkı Türkiye'den teknik olarak ileride olması ve tecrübesi.",
+		"truncated": false,
+		"display_text_range": [
+			20,
+			166
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DikenComTr",
+					"name": "Diken",
+					"id": 1494034783,
+					"id_str": "1494034783",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "umit_k",
+					"name": "ümit kıvanç",
+					"id": 256665334,
+					"id_str": "256665334",
+					"indices": [
+						12,
+						19
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1252951234446864400,
+		"in_reply_to_status_id_str": "1252951234446864386",
+		"in_reply_to_user_id": 1494034783,
+		"in_reply_to_user_id_str": "1494034783",
+		"in_reply_to_screen_name": "DikenComTr",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 293,
+			"friends_count": 257,
+			"listed_count": 9,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1161,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5222,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@DikenComTr @umit_k Aynı haberi Çin geçen yerleri Türkiye ile değiştirerek de okuyabilirsiniz. Çin'in tek farkı Türkiye'den teknik olarak ileride olması ve tecrübesi."
+	},
+	{
 		"created_at": "Tue Apr 21 16:08:55 +0000 2020",
 		"id": 1252630401174175700,
 		"id_str": "1252630401174175746",
