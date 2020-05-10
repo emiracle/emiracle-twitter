e04af7cbc1a2e959f47ce1b8e47c4019d99702d0
emiracle <ekaymakoglu@gmail.com>
2020-05-10 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6364,
-	"created_at": "Sat May 09 01:16:55 +0000 2020",
+	"tweets": 6365,
+	"created_at": "Sun May 10 01:16:55 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,109 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sat May 09 09:22:13 +0000 2020",
+		"id": 1259051035676618800,
+		"id_str": "1259051035676618753",
+		"full_text": "@yalinalpay @gazeteduvar Yazıda, en baştaki “Puşt-truth“ gördüğüm en anlamlı typo'lardan biri 😊",
+		"truncated": false,
+		"display_text_range": [
+			25,
+			95
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "yalinalpay",
+					"name": "Yalın Alpay",
+					"id": 82624033,
+					"id_str": "82624033",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "gazeteduvar",
+					"name": "duvaR",
+					"id": 743405148240773100,
+					"id_str": "743405148240773120",
+					"indices": [
+						12,
+						24
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://www.samruston.co.uk\" rel=\"nofollow\">Flamingo for Android</a>",
+		"in_reply_to_status_id": 1258979483618549800,
+		"in_reply_to_status_id_str": "1258979483618549760",
+		"in_reply_to_user_id": 82624033,
+		"in_reply_to_user_id_str": "82624033",
+		"in_reply_to_screen_name": "yalinalpay",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 291,
+			"friends_count": 257,
+			"listed_count": 9,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1180,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5239,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@yalinalpay @gazeteduvar Yazıda, en baştaki “Puşt-truth“ gördüğüm en anlamlı typo'lardan biri 😊"
+	},
+	{
 		"created_at": "Fri May 08 21:31:47 +0000 2020",
 		"id": 1258872249806999600,
 		"id_str": "1258872249806999552",
