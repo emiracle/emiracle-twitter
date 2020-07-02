a5b1b7bba1848b8ff04545952f277eaaba03720b
emiracle <ekaymakoglu@gmail.com>
2020-07-02 04:16:57 +0300

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6399,
-	"created_at": "Wed Jul 01 01:16:55 +0000 2020",
+	"tweets": 6401,
+	"created_at": "Thu Jul 02 01:16:56 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_07.js
@@ -0,0 +1,199 @@
+Grailbird.data.tweets_2020_07 = 
+[
+	{
+		"created_at": "Wed Jul 01 20:52:24 +0000 2020",
+		"id": 1278431282683314200,
+		"id_str": "1278431282683314177",
+		"full_text": "@canonduygu Avrupa futbolunu takipten koptum ama bu en sevdiğim f̶o̶r̶v̶e̶t̶ tipidir. (Baktım, forvet değilmiş; tweet'im geçerliliğini yitirdi ama sen ne demek istediğimi anlamışsındır diye düşünüyorum 😛)",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			204
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "canonduygu",
+					"name": "can önduygu",
+					"id": 94771214,
+					"id_str": "94771214",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1278426218732433400,
+		"in_reply_to_status_id_str": "1278426218732433409",
+		"in_reply_to_user_id": 94771214,
+		"in_reply_to_user_id_str": "94771214",
+		"in_reply_to_screen_name": "canonduygu",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 287,
+			"friends_count": 259,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1226,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5287,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@canonduygu Avrupa futbolunu takipten koptum ama bu en sevdiğim f̶o̶r̶v̶e̶t̶ tipidir. (Baktım, forvet değilmiş; tweet'im geçerliliğini yitirdi ama sen ne demek istediğimi anlamışsındır diye düşünüyorum 😛)"
+	},
+	{
+		"created_at": "Wed Jul 01 20:46:05 +0000 2020",
+		"id": 1278429692165488600,
+		"id_str": "1278429692165488641",
+		"full_text": "@tokcem @liberaLDP Ülkede \"büyükbaş\" eksik olmuyor hiçbir dönemde. Sığırlar Cumhuriyeti...",
+		"truncated": false,
+		"display_text_range": [
+			19,
+			90
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "tokcem",
+					"name": "CEM TOKER🐬",
+					"id": 78411332,
+					"id_str": "78411332",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"screen_name": "liberaLDP",
+					"name": "Liberal Demokrat Parti",
+					"id": 70586612,
+					"id_str": "70586612",
+					"indices": [
+						8,
+						18
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1278429179051114500,
+		"in_reply_to_status_id_str": "1278429179051114497",
+		"in_reply_to_user_id": 78411332,
+		"in_reply_to_user_id_str": "78411332",
+		"in_reply_to_screen_name": "tokcem",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 287,
+			"friends_count": 259,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1226,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5287,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@tokcem @liberaLDP Ülkede \"büyükbaş\" eksik olmuyor hiçbir dönemde. Sığırlar Cumhuriyeti..."
+	}
+]
