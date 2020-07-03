7d32d680408ea8ee32ed43eb921c525836d9c55c
emiracle <ekaymakoglu@gmail.com>
2020-07-03 04:16:56 +0300

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6401,
-	"created_at": "Thu Jul 02 01:16:56 +0000 2020",
+	"tweets": 6403,
+	"created_at": "Fri Jul 03 01:16:55 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,322 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Thu Jul 02 23:05:51 +0000 2020",
+		"id": 1278795582635683800,
+		"id_str": "1278827254953447425",
+		"full_text": "Saldırdılar, iftira attılar, KUMPAS kurdular...\n\nFenerbahçe düşmanları her zaman olduğu gibi #SarıLacivertDuvar'a çarptılar! \n\n#3TemmuzKumpası https://t.co/lHW4Xp6Rh3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			142
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SarıLacivertDuvar",
+					"indices": [
+						93,
+						111
+					]
+				},
+				{
+					"text": "3TemmuzKumpası",
+					"indices": [
+						127,
+						142
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1278754218376147000,
+					"id_str": "1278754218376146945",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/media/Eb8M-U2XYAEXIdQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eb8M-U2XYAEXIdQ.jpg",
+					"url": "https://t.co/lHW4Xp6Rh3",
+					"display_url": "pic.twitter.com/lHW4Xp6Rh3",
+					"expanded_url": "https://twitter.com/Fenerbahce/status/1278795582635683843/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1140,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 668,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 379,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1278754218376147000,
+					"id_str": "1278754218376146945",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/media/Eb8M-U2XYAEXIdQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eb8M-U2XYAEXIdQ.jpg",
+					"url": "https://t.co/lHW4Xp6Rh3",
+					"display_url": "pic.twitter.com/lHW4Xp6Rh3",
+					"expanded_url": "https://twitter.com/Fenerbahce/status/1278795582635683843/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1140,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 668,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 379,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 137771,
+						"variants": [
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1278754218376146945/vid/480x270/N9_kSYh4qKsrcu0c.mp4?tag=13"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1278754218376146945/vid/640x360/RkDk0Yll2rA2a5s9.mp4?tag=13"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1278754218376146945/vid/1280x720/VCfOB6sHkgieLaLF.mp4?tag=13"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1278754218376146945/pl/d-kDVLjUnZ6hBDSa.m3u8?tag=13"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 11525172,
+			"id_str": "11525172",
+			"name": "Fenerbahçe SK",
+			"screen_name": "Fenerbahce",
+			"location": "Kadıköy, İstanbul",
+			"description": "Fenerbahçe Spor Kulübü resmi hesabı. The official account of Fenerbahçe SK. Please follow @Fenerbahce_EN for English.",
+			"url": "https://t.co/UChceCPj66",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UChceCPj66",
+							"expanded_url": "https://fenerbahce.org/fenerol",
+							"display_url": "fenerbahce.org/fenerol",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7403474,
+			"friends_count": 91,
+			"listed_count": 3933,
+			"created_at": "Wed Dec 26 07:40:58 +0000 2007",
+			"favourites_count": 3,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 64459,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3D3936",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1235849718103560197/FWEhX0Wg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1235849718103560197/FWEhX0Wg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/11525172/1592894508",
+			"profile_link_color": "002D72",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FFF305",
+			"profile_text_color": "101080",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15047,
+		"favorite_count": 39893,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "Saldırdılar, iftira attılar, KUMPAS kurdular...\n\nFenerbahçe düşmanları her zaman olduğu gibi #SarıLacivertDuvar'a çarptılar! \n\n#3TemmuzKumpası https://t.co/lHW4Xp6Rh3"
+	},
+	{
+		"created_at": "Thu Jul 02 09:27:14 +0000 2020",
+		"id": 1278621240878674000,
+		"id_str": "1278621240878673920",
+		"full_text": "@ccanozz Verecek ışığı olmayan karanlığı sever.",
+		"truncated": false,
+		"display_text_range": [
+			9,
+			47
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ccanozz",
+					"name": "Can Öz",
+					"id": 111779992,
+					"id_str": "111779992",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://www.samruston.co.uk\" rel=\"nofollow\">Flamingo for Android</a>",
+		"in_reply_to_status_id": 1278621078693326800,
+		"in_reply_to_status_id_str": "1278621078693326849",
+		"in_reply_to_user_id": 111779992,
+		"in_reply_to_user_id_str": "111779992",
+		"in_reply_to_screen_name": "ccanozz",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 287,
+			"friends_count": 259,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1228,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5290,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@ccanozz Verecek ışığı olmayan karanlığı sever."
+	},
+	{
 		"created_at": "Wed Jul 01 20:52:24 +0000 2020",
 		"id": 1278431282683314200,
 		"id_str": "1278431282683314177",
