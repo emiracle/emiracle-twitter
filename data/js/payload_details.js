a98032c7a1dc0e2334eeb2b14512249114412b41
emiracle <ekaymakoglu@gmail.com>
2020-11-08 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6515,
-	"created_at": "Thu Nov 05 01:16:56 +0000 2020",
+	"tweets": 6516,
+	"created_at": "Sun Nov 08 01:16:55 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Sat Nov 07 21:35:33 +0000 2020",
+		"id": 1325176923471765500,
+		"id_str": "1325190177497608193",
+		"full_text": "A brief history of American Presidents - 👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👨🏾🤡👴🏼.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			133
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 392884284,
+			"id_str": "392884284",
+			"name": "James J. Zogby",
+			"screen_name": "jjz1600",
+			"location": "Washington, DC",
+			"description": "Founder @AAIUSA; Managing Dir. @Zogby_Research; Fellow @thesandersinst; ResCom @theDemocrats. Tweets are my own.",
+			"url": "https://t.co/myTIs9Amo5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/myTIs9Amo5",
+							"expanded_url": "http://www.zogbyresearchservices.com",
+							"display_url": "zogbyresearchservices.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 30460,
+			"friends_count": 60,
+			"listed_count": 281,
+			"created_at": "Mon Oct 17 18:18:38 +0000 2011",
+			"favourites_count": 4372,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 10648,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/745403641490710528/Ha8fBgah_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/745403641490710528/Ha8fBgah_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/392884284/1466612657",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 816,
+		"favorite_count": 3713,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "A brief history of American Presidents - 👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👴🏼👨🏾🤡👴🏼."
+	},
+	{
 		"created_at": "Wed Nov 04 14:43:34 +0000 2020",
 		"id": 1323999334334189600,
 		"id_str": "1323999334334189571",
