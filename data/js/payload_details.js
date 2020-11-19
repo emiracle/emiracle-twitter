23d6ee2dbc7e7c9dc8f5d571d2b3f11efd64f7cd
emiracle <ekaymakoglu@gmail.com>
2020-11-19 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6528,
-	"created_at": "Wed Nov 18 01:16:55 +0000 2020",
+	"tweets": 6529,
+	"created_at": "Thu Nov 19 01:16:55 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Thu Nov 19 01:12:34 +0000 2020",
+		"id": 1329231055476416500,
+		"id_str": "1329231055476416512",
+		"full_text": "@afsinkum Biraz önce Sıcak Kafa'nın ilk bölümünü okudum. Ne kadar da bugünden bahsediyor. Ürpertici bir benzerlik. Ürpertici ama heyecanı da diri tutuyor. Çok beğendim, tebrikler 👍",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			180
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "afsinkum",
+					"name": "Afşin Kum",
+					"id": 154225123,
+					"id_str": "154225123",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 154225123,
+		"in_reply_to_user_id_str": "154225123",
+		"in_reply_to_screen_name": "afsinkum",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 296,
+			"friends_count": 267,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1334,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5447,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@afsinkum Biraz önce Sıcak Kafa'nın ilk bölümünü okudum. Ne kadar da bugünden bahsediyor. Ürpertici bir benzerlik. Ürpertici ama heyecanı da diri tutuyor. Çok beğendim, tebrikler 👍"
+	},
+	{
 		"created_at": "Tue Nov 17 23:46:18 +0000 2020",
 		"id": 1328846960854528000,
 		"id_str": "1328846960854528000",
