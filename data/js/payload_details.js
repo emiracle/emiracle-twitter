3e5b111ad408e6200a61b88d75de22453bf8e504
emiracle <ekaymakoglu@gmail.com>
2020-11-26 04:16:57 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6541,
-	"created_at": "Wed Nov 25 01:16:56 +0000 2020",
+	"tweets": 6542,
+	"created_at": "Thu Nov 26 01:16:55 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,109 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Wed Nov 25 16:37:04 +0000 2020",
+		"id": 1331638041132208000,
+		"id_str": "1331638041132208128",
+		"full_text": "@kkarsan @PrimeVideo İçerikler konusunda haklısın ama 8 lirayı sadece Prime Video'ya vermiyoruz. Hızlı kargo/özel indirimler, Twitch fırsatları vs. de var fiyatın içerisinde.",
+		"truncated": false,
+		"display_text_range": [
+			21,
+			174
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "kkarsan",
+					"name": "Kaan Karsan",
+					"id": 65717865,
+					"id_str": "65717865",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "PrimeVideo",
+					"name": "Prime Video",
+					"id": 17220817,
+					"id_str": "17220817",
+					"indices": [
+						9,
+						20
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1331633735901573000,
+		"in_reply_to_status_id_str": "1331633735901573125",
+		"in_reply_to_user_id": 65717865,
+		"in_reply_to_user_id_str": "65717865",
+		"in_reply_to_screen_name": "kkarsan",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 297,
+			"friends_count": 270,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1340,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5466,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@kkarsan @PrimeVideo İçerikler konusunda haklısın ama 8 lirayı sadece Prime Video'ya vermiyoruz. Hızlı kargo/özel indirimler, Twitch fırsatları vs. de var fiyatın içerisinde."
+	},
+	{
 		"created_at": "Tue Nov 24 11:41:09 +0000 2020",
 		"id": 1331201182765871000,
 		"id_str": "1331201182765871104",
