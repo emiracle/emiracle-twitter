223e86715d166c0adae7831e479bad7eae964c41
emiracle <ekaymakoglu@gmail.com>
2020-12-03 04:16:56 +0300

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6550,
-	"created_at": "Tue Dec 01 01:16:55 +0000 2020",
+	"tweets": 6552,
+	"created_at": "Thu Dec 03 01:16:55 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_12.js
@@ -0,0 +1,219 @@
+Grailbird.data.tweets_2020_12 = 
+[
+	{
+		"created_at": "Wed Dec 02 16:56:03 +0000 2020",
+		"id": 1334179535529648000,
+		"id_str": "1334179535529648130",
+		"full_text": "@HevesBaltasi @esma_yildiz @mserdark @Dopigo_Official Sonra Nasrettin Hoca demiş ki... Sen de haklısın, sen de haklısın, hatun, sen de haklısın!",
+		"truncated": false,
+		"display_text_range": [
+			54,
+			144
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "HevesBaltasi",
+					"name": "Heves Baltası",
+					"id": 1300299666299523000,
+					"id_str": "1300299666299523073",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "esma_yildiz",
+					"name": "ESMA YILDIZ",
+					"id": 2617786416,
+					"id_str": "2617786416",
+					"indices": [
+						14,
+						26
+					]
+				},
+				{
+					"screen_name": "mserdark",
+					"name": "M. Serdar Kuzuloğlu",
+					"id": 15263572,
+					"id_str": "15263572",
+					"indices": [
+						27,
+						36
+					]
+				},
+				{
+					"screen_name": "Dopigo_Official",
+					"name": "Dopigo",
+					"id": 834860851974828000,
+					"id_str": "834860851974828033",
+					"indices": [
+						37,
+						53
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1334176897677336600,
+		"in_reply_to_status_id_str": "1334176897677336576",
+		"in_reply_to_user_id": 1300299666299523000,
+		"in_reply_to_user_id_str": "1300299666299523073",
+		"in_reply_to_screen_name": "HevesBaltasi",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 298,
+			"friends_count": 271,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1344,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5478,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@HevesBaltasi @esma_yildiz @mserdark @Dopigo_Official Sonra Nasrettin Hoca demiş ki... Sen de haklısın, sen de haklısın, hatun, sen de haklısın!"
+	},
+	{
+		"created_at": "Wed Dec 02 16:48:49 +0000 2020",
+		"id": 1334177714669023200,
+		"id_str": "1334177714669023234",
+		"full_text": "@emrec_f Bugün ben de çıktım. Çok soğuk. Vilmalılar her zaman abartıyor ama, o ayrı. Benim de biraz artırmam lazım ısıyı ama para yazıyor para!",
+		"truncated": false,
+		"display_text_range": [
+			9,
+			143
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "emrec_f",
+					"name": "emre",
+					"id": 93204546,
+					"id_str": "93204546",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1334082182529376300,
+		"in_reply_to_status_id_str": "1334082182529376256",
+		"in_reply_to_user_id": 93204546,
+		"in_reply_to_user_id_str": "93204546",
+		"in_reply_to_screen_name": "emrec_f",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 298,
+			"friends_count": 271,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1344,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5478,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@emrec_f Bugün ben de çıktım. Çok soğuk. Vilmalılar her zaman abartıyor ama, o ayrı. Benim de biraz artırmam lazım ısıyı ama para yazıyor para!"
+	}
+]
