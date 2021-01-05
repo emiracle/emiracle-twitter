5834eec032c8737c1de5570b21cb021736211382
emiracle <ekaymakoglu@gmail.com>
2021-01-05 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6577,
-	"created_at": "Sun Jan 03 01:16:55 +0000 2021",
+	"tweets": 6578,
+	"created_at": "Tue Jan 05 01:16:55 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,279 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Mon Jan 04 18:07:45 +0000 2021",
+		"id": 1346156377492881400,
+		"id_str": "1346156377492881408",
+		"full_text": "Cahil aptallarla taşak geçmenin yaşı olmaz gerçi. https://t.co/dyjmUVwfZD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			49
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/dyjmUVwfZD",
+					"expanded_url": "https://twitter.com/GretaThunberg/status/1345759558124384258",
+					"display_url": "twitter.com/GretaThunberg/…",
+					"indices": [
+						50,
+						73
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 301,
+			"friends_count": 273,
+			"listed_count": 8,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1376,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5512,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1345759558124384300,
+		"quoted_status_id_str": "1345759558124384258",
+		"quoted_status_permalink": {
+			"url": "https://t.co/dyjmUVwfZD",
+			"expanded": "https://twitter.com/GretaThunberg/status/1345759558124384258",
+			"display": "twitter.com/GretaThunberg/…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Jan 03 15:50:56 +0000 2021",
+			"id": 1345759558124384300,
+			"id_str": "1345759558124384258",
+			"full_text": "Thank you so much for all the well-wishes on my 18th birthday!\n\nTonight you will find me down at the local pub exposing all the dark secrets behind the climate- and school strike conspiracy and my evil handlers who can no longer control me!\nI am free at last!! https://t.co/w1VBG83VVN",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				260
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1345759552445276200,
+						"id_str": "1345759552445276163",
+						"indices": [
+							261,
+							284
+						],
+						"media_url": "http://pbs.twimg.com/media/Eq0ZLsyW4AMPPFW.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Eq0ZLsyW4AMPPFW.jpg",
+						"url": "https://t.co/w1VBG83VVN",
+						"display_url": "pic.twitter.com/w1VBG83VVN",
+						"expanded_url": "https://twitter.com/GretaThunberg/status/1345759558124384258/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 489,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1474,
+								"h": 2048,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 864,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1345759552445276200,
+						"id_str": "1345759552445276163",
+						"indices": [
+							261,
+							284
+						],
+						"media_url": "http://pbs.twimg.com/media/Eq0ZLsyW4AMPPFW.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Eq0ZLsyW4AMPPFW.jpg",
+						"url": "https://t.co/w1VBG83VVN",
+						"display_url": "pic.twitter.com/w1VBG83VVN",
+						"expanded_url": "https://twitter.com/GretaThunberg/status/1345759558124384258/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 489,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1474,
+								"h": 2048,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 864,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1006419421244678100,
+				"id_str": "1006419421244678144",
+				"name": "Greta Thunberg",
+				"screen_name": "GretaThunberg",
+				"location": "Sverige",
+				"description": "Climate and environmental activist with Asperger’s \nBorn at 375ppm\n#ClimateStrike #FridaysForFuture #SchoolStrike4Climate #FaceTheClimateEmergency",
+				"url": "https://t.co/LA8lYvULhX",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/LA8lYvULhX",
+								"expanded_url": "http://ClimateEmergencyEU.org",
+								"display_url": "ClimateEmergencyEU.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 4478627,
+				"friends_count": 2281,
+				"listed_count": 9580,
+				"created_at": "Tue Jun 12 06:14:23 +0000 2018",
+				"favourites_count": 17843,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 8706,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1283714958681931782/KLhY7JDB_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1283714958681931782/KLhY7JDB_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1006419421244678144/1559499911",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 16650,
+			"favorite_count": 206337,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "Cahil aptallarla taşak geçmenin yaşı olmaz gerçi. https://t.co/dyjmUVwfZD"
+	},
+	{
 		"created_at": "Sat Jan 02 11:27:11 +0000 2021",
 		"id": 1345330795393802200,
 		"id_str": "1345330795393802240",
