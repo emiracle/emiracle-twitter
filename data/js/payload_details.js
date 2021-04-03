25db0d821b375ae0f7d8f6d09e58c5385927b54a
emiracle <ekaymakoglu@gmail.com>
2021-04-03 04:16:55 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6664,
-	"created_at": "Thu Apr 01 01:16:55 +0000 2021",
+	"tweets": 6665,
+	"created_at": "Sat Apr 03 01:16:55 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_04.js
@@ -0,0 +1,175 @@
+Grailbird.data.tweets_2021_04 = 
+[
+	{
+		"created_at": "Fri Apr 02 12:28:22 +0000 2021",
+		"id": 1377961103041437700,
+		"id_str": "1377961103041437700",
+		"full_text": "@yildiraybeyy Biz o yollardan geçtik. Üzme kendini, bu saydıklarının gazoz kapağı kadar bilinci yok ki birleşsinler. https://t.co/Gr9obVRoAD",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			116
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "yildiraybeyy",
+					"name": "Yıldıray",
+					"id": 93829477,
+					"id_str": "93829477",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1377960546675331000,
+					"id_str": "1377960546675331074",
+					"indices": [
+						117,
+						140
+					],
+					"media_url": "http://pbs.twimg.com/media/Ex9_0QMWEAIilE7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ex9_0QMWEAIilE7.jpg",
+					"url": "https://t.co/Gr9obVRoAD",
+					"display_url": "pic.twitter.com/Gr9obVRoAD",
+					"expanded_url": "https://twitter.com/emiracle/status/1377961103041437700/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 300,
+							"h": 265,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 300,
+							"h": 265,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 300,
+							"h": 265,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1377960546675331000,
+					"id_str": "1377960546675331074",
+					"indices": [
+						117,
+						140
+					],
+					"media_url": "http://pbs.twimg.com/media/Ex9_0QMWEAIilE7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ex9_0QMWEAIilE7.jpg",
+					"url": "https://t.co/Gr9obVRoAD",
+					"display_url": "pic.twitter.com/Gr9obVRoAD",
+					"expanded_url": "https://twitter.com/emiracle/status/1377961103041437700/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 300,
+							"h": 265,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 300,
+							"h": 265,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 300,
+							"h": 265,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1377957841802252300,
+		"in_reply_to_status_id_str": "1377957841802252288",
+		"in_reply_to_user_id": 93829477,
+		"in_reply_to_user_id_str": "93829477",
+		"in_reply_to_screen_name": "yildiraybeyy",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 314,
+			"friends_count": 275,
+			"listed_count": 7,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1436,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5623,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "@yildiraybeyy Biz o yollardan geçtik. Üzme kendini, bu saydıklarının gazoz kapağı kadar bilinci yok ki birleşsinler. https://t.co/Gr9obVRoAD"
+	}
+]
