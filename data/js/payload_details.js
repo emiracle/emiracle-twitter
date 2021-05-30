1af6aac1c1c337ff02ac1780a0ee42fc384408cc
emiracle <ekaymakoglu@gmail.com>
2021-05-30 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6712,
-	"created_at": "Fri May 28 01:16:55 +0000 2021",
+	"tweets": 6713,
+	"created_at": "Sun May 30 01:16:55 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,110 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Sat May 29 14:21:23 +0000 2021",
+		"id": 1398645649642565600,
+		"id_str": "1398645649642565634",
+		"full_text": "@StolzingWalther @mortifera Biz İskeletor Eternia'yı ele geçirecek derken, siz gizli gizli Yılan Dağı'ndaki sohbetlerin videolarını izliyordunuz. Darbeci General'in vesayetini yıkmaya çalışırken, ona paşam çekiyordunuz. Hepiniz oradaydınız be!",
+		"truncated": false,
+		"display_text_range": [
+			28,
+			243
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "StolzingWalther",
+					"name": "Walther Stolzing",
+					"id": 1153001364437569500,
+					"id_str": "1153001364437569536",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "mortifera",
+					"name": "Kutsal Bayraktar",
+					"id": 85420539,
+					"id_str": "85420539",
+					"indices": [
+						17,
+						27
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1398644438243631000,
+		"in_reply_to_status_id_str": "1398644438243631107",
+		"in_reply_to_user_id": 1153001364437569500,
+		"in_reply_to_user_id_str": "1153001364437569536",
+		"in_reply_to_screen_name": "StolzingWalther",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 312,
+			"friends_count": 281,
+			"listed_count": 7,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1501,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5685,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@StolzingWalther @mortifera Biz İskeletor Eternia'yı ele geçirecek derken, siz gizli gizli Yılan Dağı'ndaki sohbetlerin videolarını izliyordunuz. Darbeci General'in vesayetini yıkmaya çalışırken, ona paşam çekiyordunuz. Hepiniz oradaydınız be!"
+	},
+	{
 		"created_at": "Thu May 27 17:29:52 +0000 2021",
 		"id": 1397968307928588300,
 		"id_str": "1397968307928588291",
