bafda30c14aed4680ba05b37395b237da96c933e
emiracle <ekaymakoglu@gmail.com>
2021-06-04 04:16:55 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6714,
-	"created_at": "Mon May 31 01:16:55 +0000 2021",
+	"tweets": 6715,
+	"created_at": "Fri Jun 04 01:16:55 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_06.js
@@ -0,0 +1,176 @@
+Grailbird.data.tweets_2021_06 = 
+[
+	{
+		"created_at": "Thu Jun 03 14:00:20 +0000 2021",
+		"id": 1400452292646633500,
+		"id_str": "1400452292646633472",
+		"full_text": "@ccanozz Faydalı bir eser olmuş, dün ben de aldım. Turnuva için güzel bir referans kitabı işlevi de görüyor 👍🏻 https://t.co/PjqqvvedRs",
+		"truncated": false,
+		"display_text_range": [
+			9,
+			110
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ccanozz",
+					"name": "Can Öz",
+					"id": 111779992,
+					"id_str": "111779992",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1400452288032829400,
+					"id_str": "1400452288032829440",
+					"indices": [
+						111,
+						134
+					],
+					"media_url": "http://pbs.twimg.com/media/E29n76EUcAAUClQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E29n76EUcAAUClQ.jpg",
+					"url": "https://t.co/PjqqvvedRs",
+					"display_url": "pic.twitter.com/PjqqvvedRs",
+					"expanded_url": "https://twitter.com/emiracle/status/1400452292646633472/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1400452288032829400,
+					"id_str": "1400452288032829440",
+					"indices": [
+						111,
+						134
+					],
+					"media_url": "http://pbs.twimg.com/media/E29n76EUcAAUClQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E29n76EUcAAUClQ.jpg",
+					"url": "https://t.co/PjqqvvedRs",
+					"display_url": "pic.twitter.com/PjqqvvedRs",
+					"expanded_url": "https://twitter.com/emiracle/status/1400452292646633472/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1400450884417425400,
+		"in_reply_to_status_id_str": "1400450884417425419",
+		"in_reply_to_user_id": 111779992,
+		"in_reply_to_user_id_str": "111779992",
+		"in_reply_to_screen_name": "ccanozz",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 314,
+			"friends_count": 281,
+			"listed_count": 7,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1505,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5688,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "@ccanozz Faydalı bir eser olmuş, dün ben de aldım. Turnuva için güzel bir referans kitabı işlevi de görüyor 👍🏻 https://t.co/PjqqvvedRs"
+	}
+]
