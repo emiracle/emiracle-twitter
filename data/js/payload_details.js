c56b45e9c584eb7b27bba97d7fca8812e87e6fe1
emiracle <ekaymakoglu@gmail.com>
2021-07-01 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6735,
-	"created_at": "Wed Jun 30 01:16:55 +0000 2021",
+	"tweets": 6736,
+	"created_at": "Thu Jul 01 01:16:55 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,120 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Wed Jun 30 18:14:12 +0000 2021",
+		"id": 1410300653952548900,
+		"id_str": "1410300653952548870",
+		"full_text": "@Pucemargine @fgeerdink @ahval_en You were expelled many years back. So this decision proves that whole trial process was a made-up.",
+		"truncated": false,
+		"display_text_range": [
+			34,
+			132
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Pucemargine",
+					"name": "Elke Götze",
+					"id": 54308395,
+					"id_str": "54308395",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "fgeerdink",
+					"name": "Frederike Geerdink",
+					"id": 20808463,
+					"id_str": "20808463",
+					"indices": [
+						13,
+						23
+					]
+				},
+				{
+					"screen_name": "ahval_en",
+					"name": "Ahval",
+					"id": 921326512288170000,
+					"id_str": "921326512288169984",
+					"indices": [
+						24,
+						33
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1410296312482893800,
+		"in_reply_to_status_id_str": "1410296312482893825",
+		"in_reply_to_user_id": 54308395,
+		"in_reply_to_user_id_str": "54308395",
+		"in_reply_to_screen_name": "Pucemargine",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 314,
+			"friends_count": 279,
+			"listed_count": 7,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1522,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5714,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Pucemargine @fgeerdink @ahval_en You were expelled many years back. So this decision proves that whole trial process was a made-up."
+	},
+	{
 		"created_at": "Tue Jun 29 18:29:33 +0000 2021",
 		"id": 1409942126692085800,
 		"id_str": "1409942126692085762",
