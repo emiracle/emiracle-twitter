4c4cd6c1bb8c9268c2659a292b7bb892227e727a
emiracle <ekaymakoglu@gmail.com>
2021-07-20 04:16:57 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6752,
-	"created_at": "Sun Jul 18 01:16:56 +0000 2021",
+	"tweets": 6753,
+	"created_at": "Tue Jul 20 01:16:56 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,100 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Mon Jul 19 18:29:57 +0000 2021",
+		"id": 1417189985715990500,
+		"id_str": "1417189985715990536",
+		"full_text": "@ZorluMurat Çocukluğumuz evet ama artık çocuk değiliz. Rıdvan da asistlerini Hasan Vezir'e değil, siyasal islamcılara yapıyor bir süredir. Bir tek üzerindeki çubuklu forma güzel, işte çocukluk da orada kaldı.",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			208
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ZorluMurat",
+					"name": "Murat Zorlu",
+					"id": 89414147,
+					"id_str": "89414147",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1417172196582215700,
+		"in_reply_to_status_id_str": "1417172196582215684",
+		"in_reply_to_user_id": 89414147,
+		"in_reply_to_user_id_str": "89414147",
+		"in_reply_to_screen_name": "ZorluMurat",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 312,
+			"friends_count": 279,
+			"listed_count": 7,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1533,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5734,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@ZorluMurat Çocukluğumuz evet ama artık çocuk değiliz. Rıdvan da asistlerini Hasan Vezir'e değil, siyasal islamcılara yapıyor bir süredir. Bir tek üzerindeki çubuklu forma güzel, işte çocukluk da orada kaldı."
+	},
+	{
 		"created_at": "Sat Jul 17 23:22:30 +0000 2021",
 		"id": 1416507654449606700,
 		"id_str": "1416538832024096775",
