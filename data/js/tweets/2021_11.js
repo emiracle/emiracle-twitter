f46005b07a1357445f0f376600441a3f363b0e93
emiracle <ekaymakoglu@gmail.com>
2021-11-05 04:16:58 +0300

    Added 1 tweets

---
+++ data/js/tweets/2021_11.js
@@ -0,0 +1,120 @@
+Grailbird.data.tweets_2021_11 = 
+[
+	{
+		"created_at": "Thu Nov 04 20:51:01 +0000 2021",
+		"id": 1456214726795538400,
+		"id_str": "1456363375630376962",
+		"full_text": "Devleti kutsallaştırıp laf ettirmeyenler için okuma parçası:\n“Eski MİT yöneticisi Eymür: Devlet görevlileri 18 kişiyi para için öldürdü; Çiller cinayetleri biliyordur, ama Ağar kim bilir nasıl takdim ediyordur!” https://t.co/QF6FOrhXGf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			235
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/QF6FOrhXGf",
+					"expanded_url": "https://t24.com.tr/haber/eski-mit-yoneticisi-eymur-devlet-gorevlileri-18-kisiyi-para-icin-oldurdu-ciller-cinayetleri-biliyordur-ama-agar-kim-bilir-nasil-takdim-ediyordur,990583",
+					"display_url": "t24.com.tr/haber/eski-mit…",
+					"indices": [
+						212,
+						235
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 96955327,
+			"id_str": "96955327",
+			"name": "ahmet şık",
+			"screen_name": "sahmetsahmet",
+			"location": "",
+			"description": "TİP İstanbul milletvekili/gazeteci https://t.co/bXSaU3jfys ahmetinadresi@yandex.com",
+			"url": "https://t.co/h42gkQg38b",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/h42gkQg38b",
+							"expanded_url": "http://Instagram.com/ahmetsahmet",
+							"display_url": "Instagram.com/ahmetsahmet",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/bXSaU3jfys",
+							"expanded_url": "http://ahmetinadresi.org",
+							"display_url": "ahmetinadresi.org",
+							"indices": [
+								35,
+								58
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1007821,
+			"friends_count": 2553,
+			"listed_count": 1396,
+			"created_at": "Tue Dec 15 10:25:48 +0000 2009",
+			"favourites_count": 2732,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 29653,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "642D8B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/459712522409349120/c1DwHMQO_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/459712522409349120/c1DwHMQO_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/96955327/1398439202",
+			"profile_link_color": "FF0000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "7AC3EE",
+			"profile_text_color": "3D1957",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 439,
+		"favorite_count": 1650,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "Devleti kutsallaştırıp laf ettirmeyenler için okuma parçası:\n“Eski MİT yöneticisi Eymür: Devlet görevlileri 18 kişiyi para için öldürdü; Çiller cinayetleri biliyordur, ama Ağar kim bilir nasıl takdim ediyordur!” https://t.co/QF6FOrhXGf"
+	}
+]
