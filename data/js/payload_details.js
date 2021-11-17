65c1f6ea7addfeea5e3c739f1c1b2339632491f4
emiracle <ekaymakoglu@gmail.com>
2021-11-17 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6835,
-	"created_at": "Sat Nov 13 01:16:58 +0000 2021",
+	"tweets": 6836,
+	"created_at": "Wed Nov 17 01:16:55 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,120 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Tue Nov 16 20:35:46 +0000 2021",
+		"id": 1460708194540560400,
+		"id_str": "1460708194540560388",
+		"full_text": "@ccanozz @umitalan @SocratesPods Dört ayaklı tüylü bireyin NFT'si var mı?",
+		"truncated": false,
+		"display_text_range": [
+			33,
+			73
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ccanozz",
+					"name": "Can Öz",
+					"id": 111779992,
+					"id_str": "111779992",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "umitalan",
+					"name": "Ümit Alan",
+					"id": 42107400,
+					"id_str": "42107400",
+					"indices": [
+						9,
+						18
+					]
+				},
+				{
+					"screen_name": "SocratesPods",
+					"name": "Socrates Podcasts",
+					"id": 1156002159432913000,
+					"id_str": "1156002159432912897",
+					"indices": [
+						19,
+						32
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1460707793980346400,
+		"in_reply_to_status_id_str": "1460707793980346370",
+		"in_reply_to_user_id": 111779992,
+		"in_reply_to_user_id_str": "111779992",
+		"in_reply_to_screen_name": "ccanozz",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 313,
+			"friends_count": 282,
+			"listed_count": 7,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1641,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5843,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@ccanozz @umitalan @SocratesPods Dört ayaklı tüylü bireyin NFT'si var mı?"
+	},
+	{
 		"created_at": "Fri Nov 12 09:30:18 +0000 2021",
 		"id": 1459091171595243500,
 		"id_str": "1459091171595243525",
