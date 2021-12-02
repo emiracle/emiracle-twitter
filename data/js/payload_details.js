1bfcf3743afcb76a3dc7c1d0655acc281b572df5
emiracle <ekaymakoglu@gmail.com>
2021-12-02 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6841,
-	"created_at": "Tue Nov 30 01:16:55 +0000 2021",
+	"tweets": 6842,
+	"created_at": "Thu Dec 02 01:16:55 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_12.js
@@ -0,0 +1,189 @@
+Grailbird.data.tweets_2021_12 = 
+[
+	{
+		"created_at": "Wed Dec 01 10:37:50 +0000 2021",
+		"id": 1465993534524342300,
+		"id_str": "1465993534524342278",
+		"full_text": "@bombakurt 41 yaşında yaptırırsın special edition. Şunun şurasında ne kaldı. 😀 P.S: Senden iki yaş büyük olduğum için yaşlılık muhabbeti yapma ehliyetim vardır. Ekte yolluyorum. https://t.co/8aQcbdkC1j",
+		"truncated": false,
+		"display_text_range": [
+			11,
+			177
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "bombakurt",
+					"name": "Ayca 🏃🏻‍♂️🏃🏻‍♂️",
+					"id": 19867827,
+					"id_str": "19867827",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1465993526466990000,
+					"id_str": "1465993526466990083",
+					"indices": [
+						178,
+						201
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FFhBWFwWUAMumuk.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FFhBWFwWUAMumuk.jpg",
+					"url": "https://t.co/8aQcbdkC1j",
+					"display_url": "pic.twitter.com/8aQcbdkC1j",
+					"expanded_url": "https://twitter.com/emiracle/status/1465993534524342278/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 480,
+							"h": 360,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 360,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 480,
+							"h": 360,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1465993526466990000,
+					"id_str": "1465993526466990083",
+					"indices": [
+						178,
+						201
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FFhBWFwWUAMumuk.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FFhBWFwWUAMumuk.jpg",
+					"url": "https://t.co/8aQcbdkC1j",
+					"display_url": "pic.twitter.com/8aQcbdkC1j",
+					"expanded_url": "https://twitter.com/emiracle/status/1465993534524342278/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 480,
+							"h": 360,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 360,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 480,
+							"h": 360,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							4,
+							3
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/FFhBWFwWUAMumuk.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1465991625587445800,
+		"in_reply_to_status_id_str": "1465991625587445769",
+		"in_reply_to_user_id": 19867827,
+		"in_reply_to_user_id_str": "19867827",
+		"in_reply_to_screen_name": "bombakurt",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 312,
+			"friends_count": 282,
+			"listed_count": 7,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1653,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5851,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "@bombakurt 41 yaşında yaptırırsın special edition. Şunun şurasında ne kaldı. 😀 P.S: Senden iki yaş büyük olduğum için yaşlılık muhabbeti yapma ehliyetim vardır. Ekte yolluyorum. https://t.co/8aQcbdkC1j"
+	}
+]
