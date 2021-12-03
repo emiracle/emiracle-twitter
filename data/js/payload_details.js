d09b701c7862f373d1a163b01f8c6423c0680a11
emiracle <ekaymakoglu@gmail.com>
2021-12-03 04:16:55 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6842,
-	"created_at": "Thu Dec 02 01:16:55 +0000 2021",
+	"tweets": 6843,
+	"created_at": "Fri Dec 03 01:16:55 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,179 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Thu Dec 02 14:57:32 +0000 2021",
+		"id": 1466421279813259300,
+		"id_str": "1466421279813259265",
+		"full_text": "@can_grss bugün sahaftan sizin kitabınızı aldım. Sevin Abla herhalde Sevin Okyay'dır diye tahmin ettim. Sahaflarda çok imzalı kitap oluyor böyle, bu başkasına imzalı kitapları alınca sanki imzalanan kişinin kitaplığından yürütmüşüm gibi bir his hasıl oluyor 😀 https://t.co/SnuKDEcukg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			259
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "can_grss",
+					"name": "Can Gürses",
+					"id": 2213829187,
+					"id_str": "2213829187",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1466421276982005800,
+					"id_str": "1466421276982005764",
+					"indices": [
+						260,
+						283
+					],
+					"media_url": "http://pbs.twimg.com/media/FFnGYcmWUAQq71F.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FFnGYcmWUAQq71F.jpg",
+					"url": "https://t.co/SnuKDEcukg",
+					"display_url": "pic.twitter.com/SnuKDEcukg",
+					"expanded_url": "https://twitter.com/emiracle/status/1466421279813259265/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1466421276982005800,
+					"id_str": "1466421276982005764",
+					"indices": [
+						260,
+						283
+					],
+					"media_url": "http://pbs.twimg.com/media/FFnGYcmWUAQq71F.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FFnGYcmWUAQq71F.jpg",
+					"url": "https://t.co/SnuKDEcukg",
+					"display_url": "pic.twitter.com/SnuKDEcukg",
+					"expanded_url": "https://twitter.com/emiracle/status/1466421279813259265/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 2213829187,
+		"in_reply_to_user_id_str": "2213829187",
+		"in_reply_to_screen_name": "can_grss",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 313,
+			"friends_count": 284,
+			"listed_count": 7,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1652,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5852,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "@can_grss bugün sahaftan sizin kitabınızı aldım. Sevin Abla herhalde Sevin Okyay'dır diye tahmin ettim. Sahaflarda çok imzalı kitap oluyor böyle, bu başkasına imzalı kitapları alınca sanki imzalanan kişinin kitaplığından yürütmüşüm gibi bir his hasıl oluyor 😀 https://t.co/SnuKDEcukg"
+	},
+	{
 		"created_at": "Wed Dec 01 10:37:50 +0000 2021",
 		"id": 1465993534524342300,
 		"id_str": "1465993534524342278",
@@ -16,7 +189,7 @@ Grailbird.data.tweets_2021_12 =
 			"user_mentions": [
 				{
 					"screen_name": "bombakurt",
-					"name": "Ayca 🏃🏻‍♂️🏃🏻‍♂️",
+					"name": "Ayca 🏃🏻♂️🏃🏻♂️",
 					"id": 19867827,
 					"id_str": "19867827",
 					"indices": [
