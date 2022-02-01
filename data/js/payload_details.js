db8e9164e86caf85b06b578dd887bc35155a4937
emiracle <ekaymakoglu@gmail.com>
2022-02-01 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6883,
-	"created_at": "Fri Jan 28 01:16:55 +0000 2022",
+	"tweets": 6884,
+	"created_at": "Tue Feb 01 01:16:55 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,102 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Mon Jan 31 18:38:20 +0000 2022",
+		"id": 1069674780826071000,
+		"id_str": "1488220116328366096",
+		"full_text": "I'm old enough to remember when the Internet wasn't a group of five websites, each consisting of screenshots of text from the other four.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			137
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 69190199,
+			"id_str": "69190199",
+			"name": "Tom Eastman",
+			"screen_name": "tveastman",
+			"location": "Wellington, New Zealand",
+			"description": "\"There's no shit pit in hell that's deep enough. Monster.\" - Nat Torkington",
+			"url": "https://t.co/ao0j2iBjHv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ao0j2iBjHv",
+							"expanded_url": "http://tom.eastman.nz",
+							"display_url": "tom.eastman.nz",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3173,
+			"friends_count": 2,
+			"listed_count": 95,
+			"created_at": "Thu Aug 27 03:36:44 +0000 2009",
+			"favourites_count": 115,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 28561,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1317167254643994624/mUvbenhx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1317167254643994624/mUvbenhx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/69190199/1451613552",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 11040,
+		"favorite_count": 68139,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "I'm old enough to remember when the Internet wasn't a group of five websites, each consisting of screenshots of text from the other four."
+	},
+	{
 		"created_at": "Thu Jan 27 18:08:43 +0000 2022",
 		"id": 1486763111826837500,
 		"id_str": "1486763111826837509",
