c243e3803a01d99f10173fb6413a8d20650fd553
emiracle <ekaymakoglu@gmail.com>
2022-03-15 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6912,
-	"created_at": "Mon Mar 14 01:16:55 +0000 2022",
+	"tweets": 6913,
+	"created_at": "Tue Mar 15 01:16:55 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,100 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Mon Mar 14 16:35:23 +0000 2022",
+		"id": 1503409468100554800,
+		"id_str": "1503409468100554758",
+		"full_text": "@SchumiSimo Beşiktaşlıları anlatan gelmiş geçmiş en iyi tezahurat ne biliyor musun? \"Niye salak olur tüm Beşiktaşlılar...\" diye başlayıp \"Beşiktaşı tutanın ben aklını *ikeyim!\" diye biten. 😃",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			190
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SchumiSimo",
+					"name": "Schumi",
+					"id": 1188755792,
+					"id_str": "1188755792",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1503408654778781700,
+		"in_reply_to_status_id_str": "1503408654778781697",
+		"in_reply_to_user_id": 1188755792,
+		"in_reply_to_user_id_str": "1188755792",
+		"in_reply_to_screen_name": "SchumiSimo",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 308,
+			"friends_count": 287,
+			"listed_count": 7,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1733,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5943,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@SchumiSimo Beşiktaşlıları anlatan gelmiş geçmiş en iyi tezahurat ne biliyor musun? \"Niye salak olur tüm Beşiktaşlılar...\" diye başlayıp \"Beşiktaşı tutanın ben aklını *ikeyim!\" diye biten. 😃"
+	},
+	{
 		"created_at": "Sun Mar 13 16:55:39 +0000 2022",
 		"id": 1503052178453377000,
 		"id_str": "1503052178453377032",
