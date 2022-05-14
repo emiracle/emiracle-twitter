2e610488fc6f3beb01667043f735ee83cd0dbaab
emiracle <ekaymakoglu@gmail.com>
2022-05-14 04:17:02 +0300

    Added 1 tweets

--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,213 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Fri May 13 15:57:00 +0000 2022",
+		"id": 1525143077215776800,
+		"id_str": "1525143077215776768",
+		"full_text": "\"...yıllardır çağrısını yaptığımız “ortak mücadele ve güç birliği” dışında bir seçeneği kalmamıştır. Savaş, kutuplaştırma, ayrıştırma ve düşmanlaştırma siyasetinin ülkeyi herkes için yaşanmaz bir hale getirdiği gerçeğinden artık hiç kimse kaçamaz, kaçmamalıdır.\" https://t.co/hEVSZ810n6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			262
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/hEVSZ810n6",
+					"expanded_url": "https://twitter.com/HDPgenelmerkezi/status/1525102914582327296",
+					"display_url": "twitter.com/HDPgenelmerkez…",
+					"indices": [
+						263,
+						286
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 306,
+			"friends_count": 288,
+			"listed_count": 0,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1787,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5996,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1525102914582327300,
+		"quoted_status_id_str": "1525102914582327296",
+		"quoted_status_permalink": {
+			"url": "https://t.co/hEVSZ810n6",
+			"expanded": "https://twitter.com/HDPgenelmerkezi/status/1525102914582327296",
+			"display": "twitter.com/HDPgenelmerkez…"
+		},
+		"quoted_status": {
+			"created_at": "Fri May 13 13:17:24 +0000 2022",
+			"id": 1525102914582327300,
+			"id_str": "1525102914582327296",
+			"full_text": "Hukuksuzluğu ve baskıyı her alanda egemen kılan iktidara karşı ortak mücadeleyi yükseltelim\n\nEş Genel Başkanlarımız Pervin Buldan ve Mithat Sancar'ın açıklaması\n\n https://t.co/M4HU7xk1Iy",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				186
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/M4HU7xk1Iy",
+						"expanded_url": "https://hdp.org.tr/tr/hukuksuzlugu-ve-baskiyi-her-alanda-egemen-kilan-iktidara-karsi-ortak-mucadeleyi-yukseltelim/16400/#.Yn5aV-TJkNI.twitter",
+						"display_url": "hdp.org.tr/tr/hukuksuzlug…",
+						"indices": [
+							163,
+							186
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1670366851,
+				"id_str": "1670366851",
+				"name": "HDP",
+				"screen_name": "HDPgenelmerkezi",
+				"location": "",
+				"description": "Halkların Demokratik Partisi - HDP Genel Merkezi'nin kurumsal Twitter hesabı.",
+				"url": "http://t.co/wNdfDXingh",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/wNdfDXingh",
+								"expanded_url": "http://www.hdp.org.tr",
+								"display_url": "hdp.org.tr",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1176012,
+				"friends_count": 180,
+				"listed_count": 852,
+				"created_at": "Wed Aug 14 12:06:05 +0000 2013",
+				"favourites_count": 893,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 41109,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/821989688932257793/Iu7Lt4BM_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/821989688932257793/Iu7Lt4BM_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1670366851/1601902635",
+				"profile_link_color": "993399",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 36,
+			"favorite_count": 189,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "tr"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "\"...yıllardır çağrısını yaptığımız “ortak mücadele ve güç birliği” dışında bir seçeneği kalmamıştır. Savaş, kutuplaştırma, ayrıştırma ve düşmanlaştırma siyasetinin ülkeyi herkes için yaşanmaz bir hale getirdiği gerçeğinden artık hiç kimse kaçamaz, kaçmamalıdır.\" https://t.co/hEVSZ810n6"
+	},
+	{
 		"created_at": "Thu May 12 19:56:52 +0000 2022",
 		"id": 1524841055614554000,
 		"id_str": "1524841055614554139",
