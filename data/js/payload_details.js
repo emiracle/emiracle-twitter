1b1e5498e81fd11b73b588bf25e8f3e80c01bb1e
emiracle <ekaymakoglu@gmail.com>
2022-07-14 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6985,
-	"created_at": "Sun Jul 10 01:16:55 +0000 2022",
+	"tweets": 6986,
+	"created_at": "Thu Jul 14 01:16:55 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Wed Jul 13 14:31:52 +0000 2022",
+		"id": 1547227312919388200,
+		"id_str": "1547227312919388163",
+		"full_text": "İlk konser: Ray Charles (1987 - Efes Antik Tiyatrosu)\nSon konser: Prenslerin Öcü/Flört/Gogol Bordello (2022 - KüçükÇiftlik Park) https://t.co/v9nimQbgyh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			128
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/v9nimQbgyh",
+					"expanded_url": "https://twitter.com/SpotifyTurkey/status/1547217563473551361",
+					"display_url": "twitter.com/SpotifyTurkey/…",
+					"indices": [
+						129,
+						152
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 304,
+			"friends_count": 285,
+			"listed_count": 7,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1812,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6030,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/626765905989627905/CJ7Fgw1I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1547217563473551400,
+		"quoted_status_id_str": "1547217563473551361",
+		"quoted_status_permalink": {
+			"url": "https://t.co/v9nimQbgyh",
+			"expanded": "https://twitter.com/SpotifyTurkey/status/1547217563473551361",
+			"display": "twitter.com/SpotifyTurkey/…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Jul 13 13:53:07 +0000 2022",
+			"id": 1547217563473551400,
+			"id_str": "1547217563473551361",
+			"full_text": "Konser Quiz'i🎙️\n\nGittiğim ilk konser:\nGittiğim son konser:",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				58
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 365666199,
+				"id_str": "365666199",
+				"name": "Spotify Türkiye",
+				"screen_name": "SpotifyTurkey",
+				"location": "Türkiye",
+				"description": "Her an için müzik. Ücretsiz olarak dinle, keşfet ve paylaş.",
+				"url": "https://t.co/z59BCsxSH0",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/z59BCsxSH0",
+								"expanded_url": "https://www.spotify.com/tr/",
+								"display_url": "spotify.com/tr/",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 73950,
+				"friends_count": 404,
+				"listed_count": 116,
+				"created_at": "Wed Aug 31 20:34:44 +0000 2011",
+				"favourites_count": 3365,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 5682,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/881828645626576896/kTJdUF7D_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/881828645626576896/kTJdUF7D_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/365666199/1641118797",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 5,
+			"favorite_count": 62,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "tr"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "İlk konser: Ray Charles (1987 - Efes Antik Tiyatrosu)\nSon konser: Prenslerin Öcü/Flört/Gogol Bordello (2022 - KüçükÇiftlik Park) https://t.co/v9nimQbgyh"
+	},
+	{
 		"created_at": "Sat Jul 09 15:13:14 +0000 2022",
 		"id": 1545788175276195800,
 		"id_str": "1545788175276195840",
