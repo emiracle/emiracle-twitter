45c2483ed1682252d488c04d70783bcc549a58be
emiracle <ekaymakoglu@gmail.com>
2022-08-03 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6990,
-	"created_at": "Fri Jul 29 01:16:55 +0000 2022",
+	"tweets": 6991,
+	"created_at": "Wed Aug 03 01:16:55 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_08.js
@@ -0,0 +1,108 @@
+Grailbird.data.tweets_2022_08 = 
+[
+	{
+		"created_at": "Tue Aug 02 23:12:56 +0000 2022",
+		"id": 1554606203237449700,
+		"id_str": "1554606203237449728",
+		"full_text": "@H1907K O zaman o günlerin anısına bir enstantane: https://t.co/jiUat1Uas0",
+		"truncated": false,
+		"display_text_range": [
+			8,
+			74
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "H1907K",
+					"name": "Haluk Kılıç",
+					"id": 1182230679930712000,
+					"id_str": "1182230679930712065",
+					"indices": [
+						0,
+						7
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/jiUat1Uas0",
+					"expanded_url": "https://www.youtube.com/watch?v=rOHkJ5PIdjM",
+					"display_url": "youtube.com/watch?v=rOHkJ5…",
+					"indices": [
+						51,
+						74
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1554605672200814600,
+		"in_reply_to_status_id_str": "1554605672200814594",
+		"in_reply_to_user_id": 1182230679930712000,
+		"in_reply_to_user_id_str": "1182230679930712065",
+		"in_reply_to_screen_name": "H1907K",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 305,
+			"friends_count": 286,
+			"listed_count": 7,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1821,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6037,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551283482025926656/9QG8B-GM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551283482025926656/9QG8B-GM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "@H1907K O zaman o günlerin anısına bir enstantane: https://t.co/jiUat1Uas0"
+	}
+]
