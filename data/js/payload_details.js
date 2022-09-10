1547710d8235ef2ae2355dbdcb61da68893fec3d
emiracle <ekaymakoglu@gmail.com>
2022-09-10 04:16:55 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7012,
-	"created_at": "Fri Sep 09 01:16:55 +0000 2022",
+	"tweets": 7013,
+	"created_at": "Sat Sep 10 01:16:55 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,176 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Fri Sep 09 12:26:13 +0000 2022",
+		"id": 1568214187842699300,
+		"id_str": "1568214187842699264",
+		"full_text": "Stop celebrating celebrity feudalism. Make Elizabeth the last. #AbolishTheMonarchy https://t.co/blTvW9NmPZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			82
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AbolishTheMonarchy",
+					"indices": [
+						63,
+						82
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1568213356091248600,
+					"id_str": "1568213356091248640",
+					"indices": [
+						83,
+						106
+					],
+					"media_url": "http://pbs.twimg.com/media/FcNpvRXXkAAf7xI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcNpvRXXkAAf7xI.jpg",
+					"url": "https://t.co/blTvW9NmPZ",
+					"display_url": "pic.twitter.com/blTvW9NmPZ",
+					"expanded_url": "https://twitter.com/emiracle/status/1568214187842699264/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 702,
+							"h": 336,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 702,
+							"h": 336,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 325,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1568213356091248600,
+					"id_str": "1568213356091248640",
+					"indices": [
+						83,
+						106
+					],
+					"media_url": "http://pbs.twimg.com/media/FcNpvRXXkAAf7xI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcNpvRXXkAAf7xI.jpg",
+					"url": "https://t.co/blTvW9NmPZ",
+					"display_url": "pic.twitter.com/blTvW9NmPZ",
+					"expanded_url": "https://twitter.com/emiracle/status/1568214187842699264/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 702,
+							"h": 336,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 702,
+							"h": 336,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 325,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 306,
+			"friends_count": 288,
+			"listed_count": 7,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1830,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6066,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551283482025926656/9QG8B-GM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551283482025926656/9QG8B-GM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Stop celebrating celebrity feudalism. Make Elizabeth the last. #AbolishTheMonarchy https://t.co/blTvW9NmPZ"
+	},
+	{
 		"created_at": "Thu Sep 08 13:37:33 +0000 2022",
 		"id": 1567869753456197600,
 		"id_str": "1567869753456197634",
