0407bbeb74939371bfa642f504fc6883fa6318b5
emiracle <ekaymakoglu@gmail.com>
2022-10-11 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7038,
-	"created_at": "Sun Oct 09 01:16:55 +0000 2022",
+	"tweets": 7039,
+	"created_at": "Tue Oct 11 01:16:55 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,110 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Mon Oct 10 12:09:06 +0000 2022",
+		"id": 1579443904440832000,
+		"id_str": "1579443904440832007",
+		"full_text": "@kubilayyesilay @emrec_f \"Bu seferki iyiymiş kardeş, bir dahakine az koy\" mu manası?",
+		"truncated": false,
+		"display_text_range": [
+			25,
+			84
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "kubilayyesilay",
+					"name": "kubikin",
+					"id": 86535564,
+					"id_str": "86535564",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "emrec_f",
+					"name": "emre",
+					"id": 93204546,
+					"id_str": "93204546",
+					"indices": [
+						16,
+						24
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1579443632784175000,
+		"in_reply_to_status_id_str": "1579443632784175105",
+		"in_reply_to_user_id": 86535564,
+		"in_reply_to_user_id_str": "86535564",
+		"in_reply_to_screen_name": "kubilayyesilay",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 308,
+			"friends_count": 291,
+			"listed_count": 7,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1829,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6092,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551283482025926656/9QG8B-GM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551283482025926656/9QG8B-GM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@kubilayyesilay @emrec_f \"Bu seferki iyiymiş kardeş, bir dahakine az koy\" mu manası?"
+	},
+	{
 		"created_at": "Sat Oct 08 02:11:16 +0000 2022",
 		"id": 1578568678849020000,
 		"id_str": "1578568678849019904",
