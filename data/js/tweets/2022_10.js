f95e138c72cf3c656ab8590e54d0496fb39a7ff3
emiracle <ekaymakoglu@gmail.com>
2022-10-21 04:16:56 +0300

    Added 1 tweets

--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,168 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Thu Oct 20 22:48:25 +0000 2022",
+		"id": 1583228672093606000,
+		"id_str": "1583228672093605889",
+		"full_text": "YouTube da zam yapmış. https://t.co/lFcMe1V4JQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			22
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1583228048157065200,
+					"id_str": "1583228048157065217",
+					"indices": [
+						23,
+						46
+					],
+					"media_url": "http://pbs.twimg.com/media/FfjBhYDWQAElvDh.png",
+					"media_url_https": "https://pbs.twimg.com/media/FfjBhYDWQAElvDh.png",
+					"url": "https://t.co/lFcMe1V4JQ",
+					"display_url": "pic.twitter.com/lFcMe1V4JQ",
+					"expanded_url": "https://twitter.com/emiracle/status/1583228672093605889/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 633,
+							"h": 411,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 633,
+							"h": 411,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 633,
+							"h": 411,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1583228048157065200,
+					"id_str": "1583228048157065217",
+					"indices": [
+						23,
+						46
+					],
+					"media_url": "http://pbs.twimg.com/media/FfjBhYDWQAElvDh.png",
+					"media_url_https": "https://pbs.twimg.com/media/FfjBhYDWQAElvDh.png",
+					"url": "https://t.co/lFcMe1V4JQ",
+					"display_url": "pic.twitter.com/lFcMe1V4JQ",
+					"expanded_url": "https://twitter.com/emiracle/status/1583228672093605889/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 633,
+							"h": 411,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 633,
+							"h": 411,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 633,
+							"h": 411,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 308,
+			"friends_count": 292,
+			"listed_count": 7,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1832,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6098,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551283482025926656/9QG8B-GM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551283482025926656/9QG8B-GM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "YouTube da zam yapmış. https://t.co/lFcMe1V4JQ"
+	},
+	{
 		"created_at": "Tue Oct 18 12:04:21 +0000 2022",
 		"id": 1582341811607330800,
 		"id_str": "1582341811607330817",
