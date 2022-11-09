08a00028b1feda36f29af2ec538896516b1c2a9c
emiracle <ekaymakoglu@gmail.com>
2022-11-09 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7053,
-	"created_at": "Tue Nov 08 01:16:55 +0000 2022",
+	"tweets": 7054,
+	"created_at": "Wed Nov 09 01:16:55 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,179 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Tue Nov 08 16:06:39 +0000 2022",
+		"id": 1590012937040965600,
+		"id_str": "1590012937040965636",
+		"full_text": "@ohannes_klcdg Ad-Blocker olmadan Türkçe siteye girilir mi hocam? Bilgisayarın işlemcisine, RAM'ine, insanın da aklına zarar. https://t.co/PtqJVonhhZ",
+		"truncated": false,
+		"display_text_range": [
+			15,
+			125
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ohannes_klcdg",
+					"name": "ohannes kılıçdağı",
+					"id": 1478766751,
+					"id_str": "1478766751",
+					"indices": [
+						0,
+						14
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1590012524958781400,
+					"id_str": "1590012524958781440",
+					"indices": [
+						126,
+						149
+					],
+					"media_url": "http://pbs.twimg.com/media/FhDb95qXEAANW6_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhDb95qXEAANW6_.jpg",
+					"url": "https://t.co/PtqJVonhhZ",
+					"display_url": "pic.twitter.com/PtqJVonhhZ",
+					"expanded_url": "https://twitter.com/emiracle/status/1590012937040965636/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 470,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 829,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1375,
+							"h": 950,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1590012524958781400,
+					"id_str": "1590012524958781440",
+					"indices": [
+						126,
+						149
+					],
+					"media_url": "http://pbs.twimg.com/media/FhDb95qXEAANW6_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhDb95qXEAANW6_.jpg",
+					"url": "https://t.co/PtqJVonhhZ",
+					"display_url": "pic.twitter.com/PtqJVonhhZ",
+					"expanded_url": "https://twitter.com/emiracle/status/1590012937040965636/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 470,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 829,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1375,
+							"h": 950,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1590007774989398000,
+		"in_reply_to_status_id_str": "1590007774989398022",
+		"in_reply_to_user_id": 1478766751,
+		"in_reply_to_user_id_str": "1478766751",
+		"in_reply_to_screen_name": "ohannes_klcdg",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 309,
+			"friends_count": 293,
+			"listed_count": 7,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1844,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6112,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551283482025926656/9QG8B-GM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551283482025926656/9QG8B-GM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "@ohannes_klcdg Ad-Blocker olmadan Türkçe siteye girilir mi hocam? Bilgisayarın işlemcisine, RAM'ine, insanın da aklına zarar. https://t.co/PtqJVonhhZ"
+	},
+	{
 		"created_at": "Mon Nov 07 14:46:27 +0000 2022",
 		"id": 1589630363307241500,
 		"id_str": "1589630363307241474",
