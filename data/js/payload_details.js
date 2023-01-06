e3935b8ebdd6e3caafac1dee11ee5046be7a5a42
emiracle <ekaymakoglu@gmail.com>
2023-01-06 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7088,
-	"created_at": "Mon Jan 02 01:16:55 +0000 2023",
+	"tweets": 7089,
+	"created_at": "Fri Jan 06 01:16:55 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_01.js
+++ data/js/tweets/2023_01.js
@@ -1,6 +1,102 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Thu Jan 05 23:43:20 +0000 2023",
+		"id": 1610532596341002200,
+		"id_str": "1611146360543232003",
+		"full_text": "Babacan'ın vatandaşlık ve temel haklara dair önerilerine gösterilen tepkiler gösteriyor ki bu ülkeye Batılı ülkelerdeki liberal-demokratik değerler hala bol geliyor. Bu millete dayayacaksın kaba milliyetçiliği, biraz da dindarlık sosu, tamam. Fazla medeniyeti bünyesi kaldırmıyor.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			280
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 361885274,
+			"id_str": "361885274",
+			"name": "Emrah Gülsunar",
+			"screen_name": "emrahgulsunar",
+			"location": "İstanbul & Malmö",
+			"description": "Akademisyen (Dr.) I Siyaset Bilimci I @lunduniversity I EN: @GulsunarEng",
+			"url": "https://t.co/BQPeA9Ebbu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BQPeA9Ebbu",
+							"expanded_url": "https://www.instagram.com/emrahgulsunar/",
+							"display_url": "instagram.com/emrahgulsunar/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 44483,
+			"friends_count": 492,
+			"listed_count": 122,
+			"created_at": "Thu Aug 25 14:03:23 +0000 2011",
+			"favourites_count": 45862,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 23324,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1599856780388990976/HqVywT8o_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1599856780388990976/HqVywT8o_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/361885274/1567890784",
+			"profile_link_color": "3166C2",
+			"profile_sidebar_border_color": "181A1E",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 290,
+		"favorite_count": 2791,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "tr",
+		"text": "Babacan'ın vatandaşlık ve temel haklara dair önerilerine gösterilen tepkiler gösteriyor ki bu ülkeye Batılı ülkelerdeki liberal-demokratik değerler hala bol geliyor. Bu millete dayayacaksın kaba milliyetçiliği, biraz da dindarlık sosu, tamam. Fazla medeniyeti bünyesi kaldırmıyor."
+	},
+	{
 		"created_at": "Mon Jan 02 00:31:09 +0000 2023",
 		"id": 1609708842178252800,
 		"id_str": "1609708842178252801",
