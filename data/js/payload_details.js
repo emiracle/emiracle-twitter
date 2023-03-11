a72b55a0a503deee1bbbfe3c33ebae17eed75136
emiracle <ekaymakoglu@gmail.com>
2023-03-11 04:16:56 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7142,
-	"created_at": "Fri Mar 10 01:16:55 +0000 2023",
+	"tweets": 7143,
+	"created_at": "Sat Mar 11 01:16:55 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,100 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Fri Mar 10 15:24:53 +0000 2023",
+		"id": 1634213744132739000,
+		"id_str": "1634213744132739073",
+		"full_text": "@BursasporSk Oh ne âlâ memleket! Nerede bir hırsız, katil, uğursuz hemen bayrağa \"vatanseverliğe\" sığınır. Ne güzel demiş Samuel Johnson: \"Vatanseverlik alçakların son sığınağıdır\"",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			180
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BursasporSk",
+					"name": "Bursaspor",
+					"id": 208957122,
+					"id_str": "208957122",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1633930634652418000,
+		"in_reply_to_status_id_str": "1633930634652418049",
+		"in_reply_to_user_id": 208957122,
+		"in_reply_to_user_id_str": "208957122",
+		"in_reply_to_screen_name": "BursasporSk",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 307,
+			"friends_count": 293,
+			"listed_count": 7,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 1984,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6226,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551283482025926656/9QG8B-GM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551283482025926656/9QG8B-GM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1519913693",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@BursasporSk Oh ne âlâ memleket! Nerede bir hırsız, katil, uğursuz hemen bayrağa \"vatanseverliğe\" sığınır. Ne güzel demiş Samuel Johnson: \"Vatanseverlik alçakların son sığınağıdır\""
+	},
+	{
 		"created_at": "Thu Mar 09 10:58:14 +0000 2023",
 		"id": 1633784252650102800,
 		"id_str": "1633784252650102784",
