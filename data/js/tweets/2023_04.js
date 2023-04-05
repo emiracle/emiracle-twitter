ad69bd8dd8351689e22abff8e972b0cf0caba800
emiracle <ekaymakoglu@gmail.com>
2023-04-05 04:16:56 +0300

    Added 1 tweets

--- data/js/tweets/2023_04.js
+++ data/js/tweets/2023_04.js
@@ -1,6 +1,294 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Tue Apr 04 14:14:12 +0000 2023",
+		"id": 1643255652825849900,
+		"id_str": "1643255652825849859",
+		"full_text": "\"Türk sorunu\" çözülmeden bu ülkede aydınlık bir gelecek inşa etmek zor. https://t.co/fFa38KuvP8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			71
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/fFa38KuvP8",
+					"expanded_url": "https://twitter.com/yildirimansur/status/1642965134850281472",
+					"display_url": "twitter.com/yildirimansur/…",
+					"indices": [
+						72,
+						95
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 306,
+			"friends_count": 291,
+			"listed_count": 7,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 2003,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6243,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551283482025926656/9QG8B-GM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551283482025926656/9QG8B-GM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1680133035",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1642965134850281500,
+		"quoted_status_id_str": "1642965134850281472",
+		"quoted_status_permalink": {
+			"url": "https://t.co/fFa38KuvP8",
+			"expanded": "https://twitter.com/yildirimansur/status/1642965134850281472",
+			"display": "twitter.com/yildirimansur/…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Apr 03 18:59:47 +0000 2023",
+			"id": 1642965134850281500,
+			"id_str": "1642965134850281472",
+			"full_text": "Bu video bugün bursa’da çekilmiştir. Kullanılamaz hale getirilen araç amcamın aracı olup bu hale getirilmesinin tek sebebi plakasının 21 yani Diyarbakır iline ait olmasıdır. Yüzlerce kişi taş,sopa ve ağır aletlerle amcama ve araca saldırmıştır. Sırf videodan dahi şahısların… https://t.co/pLnmj2Ge3e",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				275
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1642964503460454400,
+						"id_str": "1642964503460454400",
+						"indices": [
+							276,
+							299
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1642964503460454400/pu/img/AMOK3QW6poYDqEig.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1642964503460454400/pu/img/AMOK3QW6poYDqEig.jpg",
+						"url": "https://t.co/pLnmj2Ge3e",
+						"display_url": "pic.twitter.com/pLnmj2Ge3e",
+						"expanded_url": "https://twitter.com/yildirimansur/status/1642965134850281472/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 480,
+								"h": 848,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 385,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 480,
+								"h": 848,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1642964503460454400,
+						"id_str": "1642964503460454400",
+						"indices": [
+							276,
+							299
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1642964503460454400/pu/img/AMOK3QW6poYDqEig.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1642964503460454400/pu/img/AMOK3QW6poYDqEig.jpg",
+						"url": "https://t.co/pLnmj2Ge3e",
+						"display_url": "pic.twitter.com/pLnmj2Ge3e",
+						"expanded_url": "https://twitter.com/yildirimansur/status/1642965134850281472/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 480,
+								"h": 848,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 385,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 480,
+								"h": 848,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								30,
+								53
+							],
+							"duration_millis": 13280,
+							"variants": [
+								{
+									"bitrate": 632000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1642964503460454400/pu/vid/320x564/rRAa_ZGy6PrrPtda.mp4?tag=12"
+								},
+								{
+									"bitrate": 950000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1642964503460454400/pu/vid/480x848/UnaliOxomLk0cLKW.mp4?tag=12"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1642964503460454400/pu/pl/co8jdpi7OX2HkKfM.m3u8?tag=12&container=fmp4"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 736728133303423000,
+				"id_str": "736728133303422976",
+				"name": "yıldırım",
+				"screen_name": "yildirimansur",
+				"location": "İstanbul Barosu",
+				"description": "Avukat | İühf 🎓",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 877,
+				"friends_count": 773,
+				"listed_count": 4,
+				"created_at": "Sun May 29 01:17:29 +0000 2016",
+				"favourites_count": 6975,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 319,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1426226388063956994/hazH91Ru_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1426226388063956994/hazH91Ru_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/736728133303422976/1627592327",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3048,
+			"favorite_count": 8457,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "tr"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "\"Türk sorunu\" çözülmeden bu ülkede aydınlık bir gelecek inşa etmek zor. https://t.co/fFa38KuvP8"
+	},
+	{
 		"created_at": "Sat Apr 01 14:59:03 +0000 2023",
 		"id": 1642179775971172400,
 		"id_str": "1642179775971172356",
