008f37094701aaab3d5cdff678c5792839619bdb
emiracle <ekaymakoglu@gmail.com>
2023-05-07 04:16:57 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7192,
-	"created_at": "Thu May 04 01:16:55 +0000 2023",
+	"tweets": 7193,
+	"created_at": "Sun May 07 01:16:56 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_05.js
+++ data/js/tweets/2023_05.js
@@ -1,6 +1,100 @@
 Grailbird.data.tweets_2023_05 = 
 [
 	{
+		"created_at": "Sat May 06 16:35:35 +0000 2023",
+		"id": 1654887643736408000,
+		"id_str": "1654887643736408068",
+		"full_text": "@SocratesDergi Bug report: Android'te indirdim. Şifremi unuttum dedim. Mail'e gelen aktivasyon kodunu girdim, yeni şifre belirledim. O aşamadan sonra şifremin değiştine dair bir bilgilendirme almadığım gibi login de olamıyorum. Girdiğiniz mail ile kullanıcı adınız eşleşmiyor diyor. FYI.",
+		"truncated": false,
+		"display_text_range": [
+			15,
+			287
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SocratesDergi",
+					"name": "Socrates",
+					"id": 2831651580,
+					"id_str": "2831651580",
+					"indices": [
+						0,
+						14
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1654876172272951300,
+		"in_reply_to_status_id_str": "1654876172272951296",
+		"in_reply_to_user_id": 2831651580,
+		"in_reply_to_user_id_str": "2831651580",
+		"in_reply_to_screen_name": "SocratesDergi",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 307,
+			"friends_count": 293,
+			"listed_count": 7,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 2040,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6286,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551283482025926656/9QG8B-GM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551283482025926656/9QG8B-GM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1680133035",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@SocratesDergi Bug report: Android'te indirdim. Şifremi unuttum dedim. Mail'e gelen aktivasyon kodunu girdim, yeni şifre belirledim. O aşamadan sonra şifremin değiştine dair bir bilgilendirme almadığım gibi login de olamıyorum. Girdiğiniz mail ile kullanıcı adınız eşleşmiyor diyor. FYI."
+	},
+	{
 		"created_at": "Wed May 03 18:47:54 +0000 2023",
 		"id": 1653833779603972000,
 		"id_str": "1653833779603972117",
