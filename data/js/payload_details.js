d9a82d82944a0ce0b92df53234cbd0c57d2ba2e0
emiracle <ekaymakoglu@gmail.com>
2023-05-31 04:19:26 +0300

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7218,
-	"created_at": "Tue May 30 01:16:55 +0000 2023",
+	"tweets": 7219,
+	"created_at": "Wed May 31 01:19:25 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_05.js
+++ data/js/tweets/2023_05.js
@@ -1,6 +1,100 @@
 Grailbird.data.tweets_2023_05 = 
 [
 	{
+		"created_at": "Tue May 30 12:08:06 +0000 2023",
+		"id": 1663517639853875200,
+		"id_str": "1663517639853875201",
+		"full_text": "@harungunduz_ Ne tuzu kuru olacaklar, kendilerini kandırıyorlar. \"Acımadı ki acımadı ki!\" diyorlar akıllarınca. Acıyacak efendim, acıyacak. Fakirleşirken acıyacak, dayak yerken acıyacak, tutsak edilirken acıyacak. İlla ki acıyacak.",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			231
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "harungunduz_",
+					"name": "Harun Gündüz",
+					"id": 1133820035187826700,
+					"id_str": "1133820035187826689",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1663513399404142600,
+		"in_reply_to_status_id_str": "1663513399404142593",
+		"in_reply_to_user_id": 1133820035187826700,
+		"in_reply_to_user_id_str": "1133820035187826689",
+		"in_reply_to_screen_name": "harungunduz_",
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 308,
+			"friends_count": 292,
+			"listed_count": 7,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 2052,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6320,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551283482025926656/9QG8B-GM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551283482025926656/9QG8B-GM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1680133035",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "tr",
+		"text": "@harungunduz_ Ne tuzu kuru olacaklar, kendilerini kandırıyorlar. \"Acımadı ki acımadı ki!\" diyorlar akıllarınca. Acıyacak efendim, acıyacak. Fakirleşirken acıyacak, dayak yerken acıyacak, tutsak edilirken acıyacak. İlla ki acıyacak."
+	},
+	{
 		"created_at": "Mon May 29 17:25:00 +0000 2023",
 		"id": 1663194542642974700,
 		"id_str": "1663235003227029548",
