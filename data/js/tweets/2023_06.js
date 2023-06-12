d20a9874e800d07de3a72572e067c22fb2757c5a
emiracle <ekaymakoglu@gmail.com>
2023-06-12 04:16:56 +0300

    Added 1 tweets

--- data/js/tweets/2023_06.js
+++ data/js/tweets/2023_06.js
@@ -1,6 +1,281 @@
 Grailbird.data.tweets_2023_06 = 
 [
 	{
+		"created_at": "Sun Jun 11 19:50:54 +0000 2023",
+		"id": 1667982761406132200,
+		"id_str": "1667982761406132224",
+		"full_text": "9 sene sonra ilk kupa! Küçümseyecek hâlimiz yok. Yaşa Fenerbahçe 💛💙 https://t.co/ZentTcNOXv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			67
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ZentTcNOXv",
+					"expanded_url": "https://twitter.com/Fenerbahce/status/1667981109122965506",
+					"display_url": "twitter.com/Fenerbahce/sta…",
+					"indices": [
+						68,
+						91
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18937325,
+			"id_str": "18937325",
+			"name": "Emir Kaymakoğlu",
+			"screen_name": "emiracle",
+			"location": "Istanbul",
+			"description": "Tweet'lerim otobiyografik ve bedbin öğeler taşımaktadır.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 309,
+			"friends_count": 291,
+			"listed_count": 7,
+			"created_at": "Tue Jan 13 13:08:09 +0000 2009",
+			"favourites_count": 2059,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6331,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "12121A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551283482025926656/9QG8B-GM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551283482025926656/9QG8B-GM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18937325/1680133035",
+			"profile_link_color": "140DEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FAEDC8",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1667981109122965500,
+		"quoted_status_id_str": "1667981109122965506",
+		"quoted_status_permalink": {
+			"url": "https://t.co/ZentTcNOXv",
+			"expanded": "https://twitter.com/Fenerbahce/status/1667981109122965506",
+			"display": "twitter.com/Fenerbahce/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Jun 11 19:44:20 +0000 2023",
+			"id": 1667981109122965500,
+			"id_str": "1667981109122965506",
+			"full_text": "🏆 Ziraat Türkiye Kupası Fenerbahçemizin! https://t.co/xGp7M4D0MR",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				40
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1667981099677393000,
+						"id_str": "1667981099677392896",
+						"indices": [
+							41,
+							64
+						],
+						"media_url": "http://pbs.twimg.com/media/FyXb-QpWIAA1KRU.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FyXb-QpWIAA1KRU.jpg",
+						"url": "https://t.co/xGp7M4D0MR",
+						"display_url": "pic.twitter.com/xGp7M4D0MR",
+						"expanded_url": "https://twitter.com/Fenerbahce/status/1667981109122965506/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1638,
+								"h": 2048,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 960,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 544,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1667981099677393000,
+						"id_str": "1667981099677392896",
+						"indices": [
+							41,
+							64
+						],
+						"media_url": "http://pbs.twimg.com/media/FyXb-QpWIAA1KRU.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FyXb-QpWIAA1KRU.jpg",
+						"url": "https://t.co/xGp7M4D0MR",
+						"display_url": "pic.twitter.com/xGp7M4D0MR",
+						"expanded_url": "https://twitter.com/Fenerbahce/status/1667981109122965506/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1638,
+								"h": 2048,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 960,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 544,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 11525172,
+				"id_str": "11525172",
+				"name": "Fenerbahçe SK",
+				"screen_name": "Fenerbahce",
+				"location": "Kadıköy, İstanbul",
+				"description": "Fenerbahçe Spor Kulübü resmi hesabı. The official account of Fenerbahçe SK. Please follow @Fenerbahce_EN for English. #CumhuriyetinFeneri",
+				"url": "https://t.co/wJnCgE0It0",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/wJnCgE0It0",
+								"expanded_url": "https://fenerbahce.org/fenerol",
+								"display_url": "fenerbahce.org/fenerol",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 10908060,
+				"friends_count": 85,
+				"listed_count": 5339,
+				"created_at": "Wed Dec 26 07:40:58 +0000 2007",
+				"favourites_count": 1229,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 75024,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": true,
+				"profile_background_color": "3D3936",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1640331374400774145/c1oF1C6M_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1640331374400774145/c1oF1C6M_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/11525172/1684738320",
+				"profile_link_color": "002D72",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "FFF305",
+				"profile_text_color": "101080",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 19701,
+			"favorite_count": 116787,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "tr"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tr",
+		"text": "9 sene sonra ilk kupa! Küçümseyecek hâlimiz yok. Yaşa Fenerbahçe 💛💙 https://t.co/ZentTcNOXv"
+	},
+	{
 		"created_at": "Sun Jun 04 22:14:31 +0000 2023",
 		"id": 1665482186206720000,
 		"id_str": "1665482186206720001",
